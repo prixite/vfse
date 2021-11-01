4e15bd51567708f625023ad7011aa66228a1c702
Umair Khan <umair.khan@toptal.com>
2021-11-01 21:44:58 +0500

    Fix style issues

--- core/admin.py
+++ core/admin.py
@@ -1,27 +1,39 @@
 from django.contrib import admin
 
 from core import models
-from core.models import SystemImage, ManufacturerImage
+from core.models import ManufacturerImage, SystemImage
 
 
 @admin.register(models.UserModality)
 class UserModalityAdmin(admin.ModelAdmin):
-    list_display = ("user", "modality",)
+    list_display = (
+        "user",
+        "modality",
+    )
 
 
 @admin.register(models.UserSite)
 class UserSiteAdmin(admin.ModelAdmin):
-    list_display = ("user", "site",)
+    list_display = (
+        "user",
+        "site",
+    )
 
 
 @admin.register(models.UserHealthNetwork)
 class UserHealthNetworkAdmin(admin.ModelAdmin):
-    list_display = ("user", "health_network",)
+    list_display = (
+        "user",
+        "health_network",
+    )
 
 
 @admin.register(models.Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "manager",)
+    list_display = (
+        "user",
+        "manager",
+    )
 
 
 @admin.register(models.Organization)
@@ -31,7 +43,11 @@ class OrganizationAdmin(admin.ModelAdmin):
 
 @admin.register(models.Membership)
 class MembershipAdmin(admin.ModelAdmin):
-    list_display = ("user", "organization", "role",)
+    list_display = (
+        "user",
+        "organization",
+        "role",
+    )
 
 
 @admin.register(models.Site)
@@ -46,17 +62,29 @@ class ModalityAdmin(admin.ModelAdmin):
 
 @admin.register(models.System)
 class SystemAdmin(admin.ModelAdmin):
-    list_display = ("modality", "product", "ip_address",)
+    list_display = (
+        "modality",
+        "product",
+        "ip_address",
+    )
 
 
 @admin.register(models.AuditTrail)
 class AuditTrailAdmin(admin.ModelAdmin):
-    list_display = ("log", "log_type",)
+    list_display = (
+        "log",
+        "log_type",
+    )
 
 
 @admin.register(models.RemoteLoginSession)
 class RemoteLoginSessionAdmin(admin.ModelAdmin):
-    list_display = ("system", "user", "work_order", "purchase_order",)
+    list_display = (
+        "system",
+        "user",
+        "work_order",
+        "purchase_order",
+    )
 
 
 @admin.register(models.HealthNetwork)
@@ -71,22 +99,36 @@ class ManufacturerAdmin(admin.ModelAdmin):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("modality", "manufacturer", "name",)
+    list_display = (
+        "modality",
+        "manufacturer",
+        "name",
+    )
 
 
 @admin.register(models.Documentation)
 class DocumentationAdmin(admin.ModelAdmin):
-    list_display = ("product", "documenation",)
+    list_display = (
+        "product",
+        "documenation",
+    )
 
 
 @admin.register(models.Notes)
 class NotesAdmin(admin.ModelAdmin):
-    list_display = ("system", "author", "note",)
+    list_display = (
+        "system",
+        "author",
+        "note",
+    )
 
 
 @admin.register(models.Seat)
 class SeatAdmin(admin.ModelAdmin):
-    list_display = ("system", "organization",)
+    list_display = (
+        "system",
+        "organization",
+    )
 
 
 admin.site.register(SystemImage)
