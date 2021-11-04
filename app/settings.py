b94e41b9b725f8f2dafbbba6acf0298825d08961
Umair Khan <umair.khan@toptal.com>
2021-11-04 10:48:23 +0500

    Fix env

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-env = environ.Env(DEBUG=(bool, False))
+env = environ.Env(
+    DEBUG=(bool, False),
+    DATABASE_URL=(str, None),
+)
 
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
