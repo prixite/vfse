062b1265805d1e8d774a62ca484b3437e668a66f
Umair Khan <umair.khan@toptal.com>
2021-11-10 17:13:50 +0500

    Add prop types

--- frontend/src/components/TextBox/TextBox.jsx
+++ frontend/src/components/TextBox/TextBox.jsx
@@ -1,7 +1,14 @@
 import "./TextBox.scss";
+import PropTypes from 'prop-types';
 
-export default function TextBox(props) {
+function TextBox(props) {
   return (
     <input type="text" placeholder={ props.placeholder } />
   )
 }
+
+TextBox.propTypes = {
+  placeholder: PropTypes.string
+}
+
+export default TextBox;
