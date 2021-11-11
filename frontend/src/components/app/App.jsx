0898693f3c5caacf3e4aeb274f934d4f2cafbde4
Umair Khan <umair.khan@toptal.com>
2021-11-11 17:52:08 +0500

    Create Content component

--- frontend/src/components/app/App.jsx
+++ frontend/src/components/app/App.jsx
@@ -1,52 +1,24 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "./App.scss";
 import SideBar from "../sidebar/SideBar";
+import Content from "../content/Content";
 
 export default function App() {
   return (
     <React.Fragment>
       <div><Header /></div>
-      <Router>
+      <BrowserRouter>
         <div className='mid'>
           <div><SideBar /></div>
-          <div className="content">
-            <Switch>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/users">
-                <Users />
-              </Route>
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </div>
-
+          <div className="content"> <Content /> </div>
         </div>
-      </Router>
+      </BrowserRouter>
     </React.Fragment>
   );
 }
 
 function Header() {
   return <h1>vFSE</h1>;
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
---
+++ frontend/src/components/content/Content.jsx
@@ -0,0 +1,33 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+export default function Content() {
+  return (
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/users">
+                <Users />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+  )
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
