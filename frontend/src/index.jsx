c163741c73233e1f3323b6f9082da236103a6c28
Umair Khan <umair.khan@toptal.com>
2021-11-21 10:13:58 +0500

    Use cssBaseLine

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CssBaseline from '@mui/material/CssBaseline';
 
 import App from "./components/app/App";
 import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById("container")
