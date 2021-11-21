812741540fbc9a2f4a008a8991a31d0b367c6bf4
Umair Khan <umair.khan@toptal.com>
2021-11-21 10:20:09 +0500

    Add theme

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CssBaseline from '@mui/material/CssBaseline';
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material";
 
 import App from "./components/app/App";
 import "./index.scss";
+import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("container")
 );
