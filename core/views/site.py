bc217ad15a6a9cbaa31c46be610138faca060f73
Umair Khan <umair.khan@toptal.com>
2021-11-23 16:12:18 +0500

    Add css

--- core/views/site.py
+++ core/views/site.py
@@ -80,7 +80,7 @@ def duo_login(request):
 
 
 class WelcomeView(TemplateView):
-    template_name = "half_image_base.html"
+    template_name = "core/welcome.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
