ae4ada3128b0964e3defee0334c7ee5a495f6d85
Umair Khan <umair.khan@toptal.com>
2021-11-25 19:54:31 +0500

    Create AddOrganization component

--- frontend/src/views/organization/Organization.tsx
+++ frontend/src/views/organization/Organization.tsx
@@ -2,12 +2,8 @@ import { Fragment } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
+
+import AddOrganization from "@src/views/organization/AddOrganization";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
@@ -73,38 +69,5 @@ export default function Organization() {
         />
       </div>
     </Fragment>
-  );
-}
-
-function AddOrganization(props) {
-  const [value, setValue] = useState("");
-  const onChange = (event) => setValue(event.target.value);
-
-  const handleSave = (event) => {
-    props.add({ name: value });
-  };
-
-  return (
-    <Dialog open={props.open} onClose={props.handleClose}>
-      <DialogTitle>New Organization</DialogTitle>
-      <DialogContent>
-        <DialogContentText>Add new organization.</DialogContentText>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Name"
-          type="text"
-          fullWidth
-          variant="standard"
-          value={value}
-          onChange={onChange}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={props.handleClose}>Cancel</Button>
-        <Button onClick={handleSave}>Add</Button>
-      </DialogActions>
-    </Dialog>
   );
 }
