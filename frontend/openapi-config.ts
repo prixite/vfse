ce885193e45718b46f9d1e50d2883845b93ad347
Umair Khan <umair.khan@toptal.com>
2021-12-05 20:56:39 +0500

    Move api to reducers

--- frontend/openapi-config.ts
+++ frontend/openapi-config.ts
@@ -4,7 +4,7 @@ const config: ConfigFile = {
   schemaFile: "swagger.json",
   apiFile: "@src/store/emptyApi.ts",
   apiImport: "emptySplitApi",
-  outputFile: "./src/store/api.ts",
+  outputFile: "./src/store/reducers/api.ts",
   exportName: "api",
   hooks: true,
 };
