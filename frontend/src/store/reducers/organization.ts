a79a74a7bc692ee034c2cd924bbab486d1af5383
Umair Khan <umair.khan@toptal.com>
2021-12-05 21:12:43 +0500

    Action name should not be capitalized

--- frontend/src/store/reducers/organization.ts
+++ frontend/src/store/reducers/organization.ts
@@ -5,7 +5,7 @@ const initialState = {
 };
 
 export const organizationSlice = createSlice({
-  name: "Organization",
+  name: "organization",
   initialState,
   reducers: {
     setOrganizationData: (state, action) => {
