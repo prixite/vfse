fe7edf9110e855510faf4ab070cfdb031e679b38
Umair Khan <umair.khan@toptal.com>
2021-12-08 20:34:29 +0500

    Fix style

--- frontend/src/store/reducers/themeStore.ts
+++ frontend/src/store/reducers/themeStore.ts
@@ -4,16 +4,16 @@ export const themeSlice = createSlice({
   name: "themeColor",
   initialState: {
     sideBarBackground: "#2a3242",
-    buttonBackground: "#773cbe"
+    buttonBackground: "#773cbe",
   },
   reducers: {
     updateSideBarColor: (state, action) => {
-      state.sideBarBackground = action.payload.color
+      state.sideBarBackground = action.payload.color;
     },
     updateButtonColor: (state, action) => {
-      state.buttonBackground = action.payload.color
-    }
-  }
+      state.buttonBackground = action.payload.color;
+    },
+  },
 });
 
 export const { updateSideBarColor, updateButtonColor } = themeSlice.actions;
