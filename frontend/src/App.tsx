140ba51e44704611c5e02b45b0d57aa37cc09d0a
Umair Khan <umair.khan@toptal.com>
2021-12-09 12:20:09 +0500

    Fix currentOrganization redux

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import "@src/App.scss";
 import PageLayout from "@src/components/shared/Layout/PageLayout/PageLayout";
 import RoutesHOC from "@src/components/hoc/routesHOC";
 import { useMeReadQuery } from "@src/store/reducers/api";
-import { updateMe } from "@src/store/reducers/organizationStore";
+import { setCurrentOrganization } from "@src/store/reducers/organizationStore";
 import { useAppDispatch } from "@src/store/hooks";
 
 export default function App() {
@@ -11,7 +11,7 @@ export default function App() {
   const { data, isFetching } = useMeReadQuery();
 
   if (!isFetching) {
-    dispatch(updateMe(data));
+    dispatch(setCurrentOrganization({ currentOrganiation: data.organization }));
   }
 
   if (isFetching) {
