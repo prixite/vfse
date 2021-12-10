1ec87706ba2947f610e828bc1ee3c799ef342d7b
Umair Khan <umair.khan@toptal.com>
2021-12-10 07:19:40 +0500

    Fix style issues

--- core/tests/test_manufacturers.py
+++ core/tests/test_manufacturers.py
@@ -2,22 +2,25 @@ from core import models
 from core.tests import factories
 from core.tests.base import BaseTestCase
 
-class ManufacturerTestCase(BaseTestCase):
 
+class ManufacturerTestCase(BaseTestCase):
     def test_manufacturer_list(self):
         self.client.force_login(self.super_admin)
-        response = self.client.get('/api/manufacturers/')
+        response = self.client.get("/api/manufacturers/")
 
-        self.assertEqual(response.status_code,200)
-        self.assertEqual(len(response.json()),models.Manufacturer.objects.all().count())
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            len(response.json()), models.Manufacturer.objects.all().count()
+        )
 
     def test_manufacturer_post(self):
         self.client.force_login(self.super_admin)
-        response = self.client.post('/api/manufacturers/',
-        data={
-            'name':'Philips',
-            'image':factories.ManufacturerImageFactory().id
-        })
+        response = self.client.post(
+            "/api/manufacturers/",
+            data={"name": "Philips", "image": factories.ManufacturerImageFactory().id},
+        )
 
-        self.assertEqual(response.status_code,201)
-        self.assertEqual(models.Manufacturer.objects.filter(name='Philips').exists(),True)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(
+            models.Manufacturer.objects.filter(name="Philips").exists(), True
+        )
