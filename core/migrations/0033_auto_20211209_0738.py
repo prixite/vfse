2b4bf44c25b14829f12322805cee649378cab370
Umair Khan <umair.khan@toptal.com>
2021-12-10 07:13:50 +0500

    Fix black issues

--- core/migrations/0033_auto_20211209_0738.py
+++ core/migrations/0033_auto_20211209_0738.py
@@ -8,18 +8,26 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0032_auto_20211207_0703'),
+        ("core", "0032_auto_20211207_0703"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='usermodality',
-            name='user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='modalities', to=settings.AUTH_USER_MODEL),
+            model_name="usermodality",
+            name="user",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="modalities",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
         migrations.AlterField(
-            model_name='usersite',
-            name='user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='sites', to=settings.AUTH_USER_MODEL),
+            model_name="usersite",
+            name="user",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="sites",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
