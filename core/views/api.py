c124a9b286c743629d46f2eede60c78879432d24
Umair Khan <umair.khan@toptal.com>
2021-12-10 07:08:40 +0500

    Push fixes

--- core/views/api.py
+++ core/views/api.py
@@ -142,7 +142,12 @@ class UserViewSet(ModelViewSet):
             role=serializer.validated_data["role"],
             user=user,
         )
-        models.Profile.objects.filter(user=user).update(
+        self.update_profile(serializer, user.id)
+        self.add_sites(serializer, user.id)
+        self.add_modalities(serializer, user.id)
+
+    def update_profile(self, serializer, user_id):
+        models.Profile.objects.filter(user_id=user_id).update(
             **{
                 key: serializer.validated_data[key]
                 for key in [
@@ -157,13 +162,16 @@ class UserViewSet(ModelViewSet):
             }
         )
 
+    def add_sites(self, serializer, user_id):
         sites = [
-            models.UserSite(user=user, site=site)
+            models.UserSite(user_id=user_id, site=site)
             for site in serializer.validated_data["sites"]
         ]
         models.UserSite.objects.bulk_create(sites)
+
+    def add_modalities(self, serializer, user_id):
         modalities = [
-            models.UserModality(user=user, modality=modality)
+            models.UserModality(user_id=user_id, modality=modality)
             for modality in serializer.validated_data["modalities"]
         ]
         models.UserModality.objects.bulk_create(modalities)
@@ -181,34 +189,13 @@ class UserViewSet(ModelViewSet):
                 organization=serializer.validated_data["organization"],
                 role=serializer.validated_data["role"],
             )
-            models.Profile.objects.filter(user__id=kwargs["pk"]).update(
-                **{
-                    key: serializer.validated_data[key]
-                    for key in [
-                        "manager",
-                        "phone",
-                        "fse_accessible",
-                        "audit_enabled",
-                        "can_leave_notes",
-                        "view_only",
-                        "is_one_time",
-                    ]
-                }
-            )
+            self.update_profile(serializer, kwargs["pk"])
 
             models.UserSite.objects.filter(user_id=kwargs["pk"]).delete()
-            sites = [
-                models.UserSite(user_id=kwargs["pk"], site=site)
-                for site in serializer.validated_data["sites"]
-            ]
-            models.UserSite.objects.bulk_create(sites)
+            self.add_sites(serializer, kwargs["pk"])
 
             models.UserModality.objects.filter(user_id=kwargs["pk"]).delete()
-            modalities = [
-                models.UserModality(user_id=kwargs["pk"], modality=modality)
-                for modality in serializer.validated_data["modalities"]
-            ]
-            models.UserModality.objects.bulk_create(modalities)
+            self.add_modalities(serializer, kwargs["pk"])
 
             return Response(serializer.data)
         return Response(serializer.errors)
