cb7024bb0c694f1e451500a6d52e7656ee2cc867
Umair Khan <umair.khan@toptal.com>
2021-12-14 14:22:42 +0500

    Added ServerPage Error.

--- frontend/src/components/common/Presentational/OrganizationViewBtns/OrganizationViewBtns.tsx
+++ frontend/src/components/common/Presentational/OrganizationViewBtns/OrganizationViewBtns.tsx
@@ -13,54 +13,53 @@ const OrganizationViewBtns = () => {
   const constantData: any = localizedData()?.organization;
   const { btnFilter, btnAddClients } = constantData;
 
-  const {
-    buttonBackground,
-    buttonTextColor,
-  } = useAppSelector((state) => state.myTheme);
+  const { buttonBackground, buttonTextColor } = useAppSelector(
+    (state) => state.myTheme
+  );
 
   return (
     <>
-        <Box component="div" className="OrganizationSection__Header">
-          <Box component="div" className="InputSection">
-            <Button variant="contained" className="Filterbtn">
-              <div className="btn-content">
-                <FilterAltIcon style={{ marginRight: "9px" }} />
-                <span>{btnFilter}</span>
-              </div>
-            </Button>
-
-            <TextField
-              id="search-clients"
-              className="Search-input"
-              variant="outlined"
-              placeholder="Search"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Box>
-          <Button
-            style={{
-              backgroundColor: buttonBackground,
-              color: buttonTextColor,
-            }}
-            onClick={() => {
-              setOpen(true);
-              setOrganization(null);
-            }}
-            variant="contained"
-            className="AddClientsbtn"
-          >
+      <Box component="div" className="OrganizationSection__Header">
+        <Box component="div" className="InputSection">
+          <Button variant="contained" className="Filterbtn">
             <div className="btn-content">
-              <AddIcon />
-              <span>{btnAddClients}</span>
+              <FilterAltIcon style={{ marginRight: "9px" }} />
+              <span>{btnFilter}</span>
             </div>
           </Button>
+
+          <TextField
+            id="search-clients"
+            className="Search-input"
+            variant="outlined"
+            placeholder="Search"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
         </Box>
+        <Button
+          style={{
+            backgroundColor: buttonBackground,
+            color: buttonTextColor,
+          }}
+          onClick={() => {
+            setOpen(true);
+            setOrganization(null);
+          }}
+          variant="contained"
+          className="AddClientsbtn"
+        >
+          <div className="btn-content">
+            <AddIcon />
+            <span>{btnAddClients}</span>
+          </div>
+        </Button>
+      </Box>
     </>
   );
 };
