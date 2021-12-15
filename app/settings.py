22be5b031cc1fe36d07c87103884e11c4d8c3e9d
Umair Khan <umair.khan@toptal.com>
2021-12-15 12:39:29 +0500

    Revert "Disable pagination."
    This reverts commit 6e9918a9b1f0b40aed99fe63947fa4d2be319b14.

--- app/settings.py
+++ app/settings.py
@@ -178,6 +178,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_PAGINATION_CLASS": "drf_link_header_pagination.LinkHeaderPagination",
+    "PAGE_SIZE": 50,
 }
 
 
