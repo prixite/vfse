e298f1fa1253e50a191bb48f31a9f7c25e6961a1
Umair Khan <umair.khan@toptal.com>
2021-12-15 17:03:01 +0500

    Style fix

--- core/views/mixins.py
+++ core/views/mixins.py
@@ -1,11 +1,14 @@
-from core import models
 from django.db.models import Q
+
+from core import models
 
 
 class UserOganizationMixin:
     def get_user_organizations(self):
         if self.request.user.is_superuser or self.request.user.is_supermanager:
-            return models.Organization.objects.filter(Q(is_customer=True) | Q(is_default=True))
+            return models.Organization.objects.filter(
+                Q(is_customer=True) | Q(is_default=True)
+            )
         return models.Organization.objects.filter(
             id__in=self.request.user.get_organizations()
         )
