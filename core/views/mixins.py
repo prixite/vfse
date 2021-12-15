21685fb84078b305c39a39fc7511c162c906ebc2
Umair Khan <umair.khan@toptal.com>
2021-12-15 16:34:08 +0500

    Customer API

--- core/views/mixins.py
+++ core/views/mixins.py
@@ -4,7 +4,7 @@ from core import models
 class UserOganizationMixin:
     def get_user_organizations(self):
         if self.request.user.is_superuser or self.request.user.is_supermanager:
-            return models.Organization.objects.all()
+            return models.Organization.objects.filter(is_customer=True)
         return models.Organization.objects.filter(
             id__in=self.request.user.get_organizations()
         )
