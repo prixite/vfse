e57bf403003155f518bb8b27e0969b7aff18efce
Umair Khan <umair.khan@toptal.com>
2021-12-16 16:40:10 +0500

    Refactored Routes

--- core/migrations/0037_merge_20211216_0758.py
+++ core/migrations/0037_merge_20211216_0758.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0036_merge_0035_auto_20211215_1012_0035_auto_20211215_1039'),
-        ('core', '0036_organization_is_customer'),
+        ("core", "0036_merge_0035_auto_20211215_1012_0035_auto_20211215_1039"),
+        ("core", "0036_organization_is_customer"),
     ]
 
-    operations = [
-    ]
+    operations = []
