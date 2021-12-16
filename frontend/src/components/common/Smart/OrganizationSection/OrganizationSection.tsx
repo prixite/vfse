c3da118526415ab897fec919ae9ece2c9a1f4398
Umair Khan <umair.khan@toptal.com>
2021-12-16 12:30:50 +0500

    Add search functionality

--- frontend/src/components/common/Smart/OrganizationSection/OrganizationSection.tsx
+++ frontend/src/components/common/Smart/OrganizationSection/OrganizationSection.tsx
@@ -25,11 +25,14 @@ import { updateOrganizationColor } from "@src/services/organizationService";
 
 const OrganizationSection = () => {
   const [organization, setOrganization] = useState(null);
+  const [organizationsList, setOrganizationsList] = useState({});
+  const [searchText, setSearchText] = useState('');
   const [open, setOpen] = useState(false);
   const [organizationsPartialUpdate] = useOrganizationsPartialUpdateMutation();
   const { data: organizationList, refetch } = useOrganizationsListQuery({
     page: 1,
   });
+  
   const constantData: any = localizedData()?.organization;
   const { title } = constantData;
 
@@ -159,23 +162,49 @@ const OrganizationSection = () => {
           setOpen={setOpen}
           path="organizations"
           setData={setOrganization}
+          setOrganizationsList={setOrganizationsList}
+          searchText={searchText}
+          setSearchText={setSearchText}
         />
         <Grid container spacing={2} className="OrganizationSection__AllClients">
-          {organizationList &&
-            organizationList?.length &&
-            organizationList.map((item, key) => (
-              <Grid key={key} item xs={3}>
-                <ClientCard
-                  setOpen={setOpen}
-                  setOrganization={setOrganization}
-                  row={item}
-                  refetch={refetch}
-                  id={item.id}
-                  name={item.name}
-                  logo={item.logo}
-                />
-              </Grid>
-            ))}
+          {
+          searchText?.length > 2 
+          ?
+             organizationsList && 
+              organizationsList?.results?.length &&
+              organizationsList?.query === searchText ? 
+              organizationsList?.results?.map((item, key) => (
+                <Grid key={key} item xs={3}>
+                  <ClientCard
+                    setOpen={setOpen}
+                    setOrganization={setOrganization}
+                    row={item}
+                    refetch={refetch}
+                    id={item.id}
+                    name={item.name}
+                    logo={item.logo}
+                  />
+                </Grid>
+              ))
+            : organizationsList?.query === searchText ? <p style={{marginTop: '20px', marginLeft: '20px'}}>no results found</p> : ''
+          :
+              organizationList &&
+              organizationList?.length ?
+              organizationList.map((item, key) => (
+                <Grid key={key} item xs={3}>
+                  <ClientCard
+                    setOpen={setOpen}
+                    setOrganization={setOrganization}
+                    row={item}
+                    refetch={refetch}
+                    id={item.id}
+                    name={item.name}
+                    logo={item.logo}
+                  />
+                </Grid>
+              ))
+              : ''
+            }
         </Grid>
         <OrganizationModal
           organization={organization}
