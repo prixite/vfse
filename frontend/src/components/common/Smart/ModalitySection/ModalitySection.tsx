714b780aa6670170b21a3ebf6006d5f84a79007d
Umair Khan <umair.khan@toptal.com>
2021-12-16 19:42:36 +0500

    Fix structure

--- frontend/src/components/common/Smart/ModalitySection/ModalitySection.tsx
+++ frontend/src/components/common/Smart/ModalitySection/ModalitySection.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import { Box, Grid } from "@mui/material";
 import "react-toastify/dist/ReactToastify.css";
-import OrganizationModal from "@src/components/common/Smart/OrganizationModal/OrganizationModal";
-import ClientCard from "@src/components/common/Presentational/ClientCard/ClientCard";
-import TopViewBtns from "@src/components/common/Presentational/TopViewBtns/TopViewBtns";
+import NetworkModal from "@src/components/shared/popUps/NetworkModal/NetworkModal";
+import NetworkCard from "@src/components/common/Presentational/NetworkCard/NetworkCard";
+import TopViewBtns from "@src/components/common/Smart/TopViewBtns/TopViewBtns";
 import { useOrganizationsHealthNetworksListQuery } from "@src/store/reducers/api";
 
 import "@src/components/common/Smart/ModalitySection/ModalitySection.scss";
-import { useAppDispatch, useAppSelector } from "@src/store/hooks";
 import { localizedData } from "@src/helpers/utils/language";
 
 const ModalitySection = () => {
@@ -16,14 +15,10 @@ const ModalitySection = () => {
   const constantData: any = localizedData()?.modalities;
   const { title } = constantData;
 
-  const currentOrganization = useAppSelector(
-    (state) => state.organization.currentOrganization
-  );
-
   const { data: networksData, refetch: orgNetworkRefetch } =
     useOrganizationsHealthNetworksListQuery({
       page: 1,
-      organizationPk: currentOrganization.id.toString(),
+      organizationPk: "3",
     });
 
   const handleClose = () => setOpen(false);
@@ -38,7 +33,7 @@ const ModalitySection = () => {
             networksData?.length &&
             networksData.map((item, key) => (
               <Grid key={key} item xs={3}>
-                <ClientCard
+                <NetworkCard
                   setOpen={setOpen}
                   setOrganization={setNetwork}
                   row={item}
@@ -50,13 +45,7 @@ const ModalitySection = () => {
               </Grid>
             ))}
         </Grid>
-        <OrganizationModal
-          organization={network}
-          setOrganization={setNetwork}
-          open={open}
-          handleClose={handleClose}
-          refetch={orgNetworkRefetch}
-        />
+        <NetworkModal open={open} handleClose={handleClose} />
       </Box>
     </>
   );
---
+++ frontend/src/components/shared/popUps/NetworkModal/NetworkModal.tsx
@@ -0,0 +1,59 @@
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import { toast } from "react-toastify";
+import { localizedData } from "@src/helpers/utils/language";
+
+export default function NetworkModal(props) {
+  const constantData: object = localizedData()?.modalities?.popUp;
+  const {
+    popUpNewNetwork,
+    newNetworkName,
+    newNetworkSeat,
+    newNetworkBtnSave,
+    newNetworkBtnCancel,
+  } = constantData;
+
+  const handleSetNewNetwork = async () => {
+      toast.success("Organization successfully added");
+      props.handleClose();
+  };
+
+  return (
+    <Dialog open={props.open} onClose={props.handleClose}>
+      <DialogTitle>
+        {popUpNewNetwork}
+      </DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label={newNetworkName}
+          type="text"
+          fullWidth
+          value={""}
+          variant="standard"
+        />
+        <TextField
+          margin="dense"
+          id="seats"
+          label={newNetworkSeat}
+          type="text"
+          fullWidth
+          variant="standard"
+          value={0}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={props.handleClose}>{newNetworkBtnCancel}</Button>
+        <Button onClick={handleSetNewNetwork}>
+          {newNetworkBtnSave}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
