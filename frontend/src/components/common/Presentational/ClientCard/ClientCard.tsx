fc2db99dee3ae736e5ea04f929bf741bbc028105
Umair Khan <umair.khan@toptal.com>
2021-12-20 15:46:41 +0500

    Fix pop over position on cards

--- frontend/src/components/common/Presentational/ClientCard/ClientCard.tsx
+++ frontend/src/components/common/Presentational/ClientCard/ClientCard.tsx
@@ -89,6 +89,14 @@ const ClientCard = ({
           aria-labelledby="client-options-button"
           anchorEl={anchorEl}
           open={open}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
           className="dropdownMenu"
           onClose={handleClose}
         >
