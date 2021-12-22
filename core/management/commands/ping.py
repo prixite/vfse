67460887d0a86062bf000a41543035a0c912d787
Umair Khan <umair.khan@toptal.com>
2021-12-22 11:44:34 +0500

    Logic fix

--- core/management/commands/ping.py
+++ core/management/commands/ping.py
@@ -10,15 +10,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         systems = models.System.objects.filter(connection_monitoring=True)
         for system in systems:
+            system.is_online=False
             try:
                 subprocess.run(f'ping -c2 {system.ip_address}',shell=True,stdout=subprocess.PIPE,check=True)
                 system.is_online=True
                 system.last_successful_ping_at = timezone.now()
-                system.save()
                 self.stdout.write(self.style.SUCCESS(f'Ping Successfull @ {system.ip_address}'))
-            
+    
             except subprocess.CalledProcessError:
                 self.stdout.write(self.style.ERROR(f'Ping Unsuccessfull @ {system.ip_address}'))
             except subprocess.TimeoutExpired:
                 self.stdout.write(self.style.ERROR(f'Timeout Error, Ping Unsuccessfull @ {system.ip_address}'))
-                
+            finally:
+                system.save()
