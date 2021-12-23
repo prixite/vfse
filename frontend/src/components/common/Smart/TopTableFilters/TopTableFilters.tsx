77a5b911976122603e2f3608eab19925fd9011e5
Umair Khan <umair.khan@toptal.com>
2021-12-23 14:07:28 +0500

    Add documentation section

--- frontend/src/components/common/Smart/TopTableFilters/TopTableFilters.tsx
+++ frontend/src/components/common/Smart/TopTableFilters/TopTableFilters.tsx
@@ -1,56 +1,54 @@
 import { Box, Button, InputAdornment, TextField } from "@mui/material";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import SearchIcon from "@mui/icons-material/Search";
-import ArrowUpIcon from '@src/assets/svgs/arrow-up.svg'
-import ArrowDown from '@src/assets/svgs/arrow-long.svg'
+import ArrowUpIcon from "@src/assets/svgs/arrow-up.svg";
+import ArrowDown from "@src/assets/svgs/arrow-long.svg";
 import "@src/components/common/Smart/TopTableFilters/TopTableFilters.scss";
 import { localizedData } from "@src/helpers/utils/language";
 
-const TopTableFilters = (
-) => {
+const TopTableFilters = () => {
   let constantData: any;
-    constantData = localizedData()?.documentation;
+  constantData = localizedData()?.documentation;
   const { btnFilter, btnCol, btnAsset } = constantData;
-
 
   return (
     <>
       <Box component="div" className="TopTableFIlters__Header">
         <Box component="div" className="InputSection">
-          <Button
-            variant="contained"
-            className="Filterbtn"
-          >
+          <Button variant="contained" className="Filterbtn">
             <div className="btn-content">
               <FilterAltIcon style={{ marginRight: "9px" }} />
               <span>{btnFilter}</span>
             </div>
           </Button>
-          <Button
-            variant="contained"
-            className="Filterbtn"
-          >
-            <div className="btn-content" style={{textTransform: 'none'}}>
+          <Button variant="contained" className="Filterbtn">
+            <div className="btn-content" style={{ textTransform: "none" }}>
               <span>{`${btnCol} 8/14`}</span>
               <span className="coloumns-icon">
-                <img style={{transform: 'rotate(180deg)', marginBottom: '5px'}} src={ArrowUpIcon}/>
-                <img src={ArrowUpIcon}/>
+                <img
+                  style={{ transform: "rotate(180deg)", marginBottom: "5px" }}
+                  src={ArrowUpIcon}
+                />
+                <img src={ArrowUpIcon} />
               </span>
             </div>
           </Button>
-          <Button
-            variant="contained"
-            className="Filterbtn"
-          >
-            <div className="btn-content" style={{textTransform: 'none'}}>
-              <div style={{display: 'flex', flexDirection: 'column', fontSize: '11px'}}>
-                <span style={{height: '10px'}}>0</span>
+          <Button variant="contained" className="Filterbtn">
+            <div className="btn-content" style={{ textTransform: "none" }}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  fontSize: "11px",
+                }}
+              >
+                <span style={{ height: "10px" }}>0</span>
                 <span>9</span>
               </div>
-              <img src={ArrowDown} style={{marginRight: '10px'}}/>
+              <img src={ArrowDown} style={{ marginRight: "10px" }} />
               <span>{btnAsset}</span>
               <span className="coloumns-icon">
-                <img className="asset-image" src={ArrowUpIcon}/>
+                <img className="asset-image" src={ArrowUpIcon} />
               </span>
             </div>
           </Button>
