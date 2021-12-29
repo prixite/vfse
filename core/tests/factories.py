b4e29070d4f7d98c14f68895502275aba8fc1ceb
Umair Khan <umair.khan@toptal.com>
2021-12-29 10:28:35 +0500

    Post Generation Hook

--- core/tests/factories.py
+++ core/tests/factories.py
@@ -178,6 +178,14 @@ class HealthNetworkFactory(OrganizationFactory):
 
         models.OrganizationHealthNetwork.objects.bulk_create(relations)
 
+    @factory.post_generation
+    def health_network_users(obj,create,extracted,**kwargs):
+        if not create:
+            return
+        org_health_network = models.OrganizationHealthNetwork.objects.filter(health_network=obj).first()
+        models.UserHealthNetwork.objects.bulk_create(
+            [models.UserHealthNetwork(user=user,organization_health_network=org_health_network) for user in extracted]
+        )
 
 class OrganizationHealthNetworkFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -281,8 +289,3 @@ class SystemImageFactory(factory.django.DjangoModelFactory):
         model = models.SystemImage
 
     image = "http://example.com/systemimage.jpeg"
-
-
-class UserHealthNetworkFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = models.UserHealthNetwork
