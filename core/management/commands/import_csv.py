c597f815277f49c5b1b972aefb9b2d8d11a02cfb
Umair Khan <umair.khan@toptal.com>
2021-12-31 10:58:44 +0500

    Stuctural Improvements

--- core/management/commands/import_csv.py
+++ core/management/commands/import_csv.py
@@ -13,88 +13,97 @@ class Command(BaseCommand):
     @transaction.atomic
     def handle(self, *args, **options):
         with open(options["file"][0]) as csv_file:
-            appearance = {
-                "sidebar_text": "#94989E",
-                "button_text": "#FFFFFF",
-                "sidebar_color": "#142139",
-                "primary_color": "#773CBD",
-                "font_one": "helvetica",
-                "font_two": "calibri",
-                "logo": (
-                    "https://vfse.s3.us-east-2.amazonaws.com/m_vfse-3_preview.png"
-                ),
-                "banner": "http://example.com/image.jpg",
-                "icon": "http://example.com/icon.ico",
-            }
-
-            file = csv.DictReader(csv_file)
-            for row in file:
-                appearance["logo"] = row["Organization Logo URL"]
-                organization = serializers.OrganizationSerializer(
-                    data={
-                        "name": row["Organization Name"],
-                        "appearance": appearance,
-                    }
-                )
-                organization.is_valid(raise_exception=True)
-                obj = organization.save()
-                site = serializers.MetaSiteSerializer(
-                    data={
-                        "name": row["Site Name"],
-                        "address": row["Site Address"],
-                    }
-                )
-                site.is_valid(raise_exception=True)
-                obj = site.save(organization=obj)
-                manufacturer_image = models.ManufacturerImage.objects.create(
-                    image=row["Manufacturer Image URL"]
-                )
-                manufacturer = models.Manufacturer.objects.create(
-                    name=row["Manufacturer Name"], image=manufacturer_image
-                )
-                product = models.Product.objects.create(
-                    name=row["Product Name"], manufacturer=manufacturer
-                )
-                modality = models.Modality.objects.create(name=row["Modality"])
-                documentation = models.Documentation.objects.create(
-                    url=row["Documentation Link"]
-                )
-                product_model = serializers.ProductModelSerializer(
-                    data={
-                        "product": product.id,
-                        "modality": modality.id,
-                        "documentation": documentation.id,
-                    }
-                )
-                product_model.is_valid(raise_exception=True)
-                product_mdl = product_model.save()
-
-                system_image = models.SystemImage.objects.create(image=row["Image URL"])
-                system = serializers.SystemSerializer(
-                    data={
-                        "name": row["Name"],
-                        "site": obj.id,
-                        "product_model": product_mdl.id,
-                        "software_version": row["Software Version"],
-                        "asset_number": row["Asset Number"],
-                        "ip_address": row["IP Address"],
-                        "local_ae_title": row["Local AE Title"],
-                        "serial_number": row["Serial Number"],
-                        "location_in_building": row["Location in Building"],
-                        "system_contact_info": row["Contact Info"],
-                        "virtual_media_control": row["Virtual Media Control"],
-                        "service_web_browser": row["Service Web Browser"],
-                        "ssh": row["SSH"],
-                        "contection_monitoring": row["Connection Monitoring"],
-                        "image": system_image.id,
-                    }
-                )
-
-                system.is_valid(raise_exception=True)
-                system.save()
+            file_obj = csv.DictReader(csv_file)
+            for row in file_obj:
+                organization = self.add_organization(row)
+                site = self.add_site(row,organization)
+                product_model = self.add_product(row)
+                system = self.add_system(row,site,product_model)
 
             self.stdout.write(
                 self.style.SUCCESS(
                     f"Data Imported successfully imported from {csv_file.name}"
                 )
             )
+
+
+    def add_organization(self,row):
+        organization = serializers.OrganizationSerializer(
+            data={
+                "name": row["Organization Name"],
+                "appearance": {
+                "sidebar_text": "#94989E",
+                "button_text": "#FFFFFF",
+                "sidebar_color": "#142139",
+                "primary_color": "#773CBD",
+                "font_one": "helvetica",
+                "font_two": "calibri",
+                "logo": row.get('Organization Logo URL',
+                    "https://vfse.s3.us-east-2.amazonaws.com/m_vfse-3_preview.png"
+                ),
+                "banner": "http://example.com/image.jpg",
+                "icon": "http://example.com/icon.ico",
+            },
+            }
+        )
+        organization.is_valid(raise_exception=True)
+        return organization.save()
+
+    def add_site(self,row,organization):
+        site = serializers.MetaSiteSerializer(
+                    data={
+                        "name": row["Site Name"],
+                        "address": row["Site Address"],
+                    }
+                )
+        site.is_valid(raise_exception=True)
+        return site.save(organization=organization)
+
+    def add_product(self,row):
+        manufacturer_image = models.ManufacturerImage.objects.create(
+                    image=row["Manufacturer Image URL"]
+                )
+        manufacturer = models.Manufacturer.objects.create(
+            name=row["Manufacturer Name"], image=manufacturer_image
+        )
+        product = models.Product.objects.create(
+            name=row["Product Name"], manufacturer=manufacturer
+        )
+        modality = models.Modality.objects.create(name=row["Modality"])
+        documentation = models.Documentation.objects.create(
+            url=row["Documentation Link"]
+        )
+        product_model = serializers.ProductModelSerializer(
+            data={
+                "product": product.id,
+                "modality": modality.id,
+                "documentation": documentation.id,
+            }
+        )
+        product_model.is_valid(raise_exception=True)
+        return product_model.save()
+
+    def add_system(self,row,site,product_model):
+        system_image = models.SystemImage.objects.create(image=row["Image URL"])
+        system = serializers.SystemSerializer(
+            data={
+                "name": row["Name"],
+                "site": site.id,
+                "product_model": product_model.id,
+                "software_version": row["Software Version"],
+                "asset_number": row["Asset Number"],
+                "ip_address": row["IP Address"],
+                "local_ae_title": row["Local AE Title"],
+                "serial_number": row["Serial Number"],
+                "location_in_building": row["Location in Building"],
+                "system_contact_info": row["Contact Info"],
+                "virtual_media_control": row["Virtual Media Control"],
+                "service_web_browser": row["Service Web Browser"],
+                "ssh": row["SSH"],
+                "contection_monitoring": row["Connection Monitoring"],
+                "image": system_image.id,
+            }
+        )
+
+        system.is_valid(raise_exception=True)
+        system.save()
