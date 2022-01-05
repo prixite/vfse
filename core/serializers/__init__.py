82b7dd65ff0db45a92c629963054f9be9fc21713
Umair Khan <umair.khan@toptal.com>
2022-01-05 21:10:09 +0500

    Add serializers

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -285,16 +285,34 @@ class ModalitySerializer(serializers.ModelSerializer):
         fields = ["name"]
 
 
-class ProductModelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.ProductModel
-        fields = ["id", "product", "modality", "documentation"]
-
-
 class ManufacturerSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Manufacturer
         fields = ["name", "image"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    manufacturer = ManufacturerSerializer()
+
+    class Meta:
+        model = models.Product
+        fields = ["id", "name", "manufacturer"]
+
+
+class DocumentationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Documentation
+        fields = ["id", "url"]
+
+
+class ProductModelSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    modality = ModalitySerializer()
+    documentation = DocumentationSerializer()
+
+    class Meta:
+        model = models.ProductModel
+        fields = ["id", "product", "modality", "documentation"]
 
 
 class SystemNotesSerializer(serializers.ModelSerializer):
@@ -351,14 +369,6 @@ class UserRequestAcessSeriazlizer(UpsertUserSerializer):
 
     def validate_organization(self, value):
         return value
-
-
-class ProductSerializer(serializers.ModelSerializer):
-    manufacturer = ManufacturerSerializer()
-
-    class Meta:
-        model = models.Product
-        fields = ["id", "name", "manufacturer"]
 
 
 class ProductCreateSerializer(serializers.ModelSerializer):
