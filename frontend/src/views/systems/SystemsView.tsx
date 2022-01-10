f288368b0bf43807fefd509faca09f6bae7bcfc7
Umair Khan <umair.khan@toptal.com>
2022-01-10 22:27:30 +0500

    Add apis for systems.

--- frontend/src/views/systems/SystemsView.tsx
+++ frontend/src/views/systems/SystemsView.tsx
@@ -1,8 +1,16 @@
 import SectionSkeleton from "@src/components/common/Presentational/SectionSkeleton/SectionSkeleton";
 import SystemSection from "@src/components/common/Smart/SystemSection/SystemSection";
+import { useAppSelector } from "@src/store/hooks";
 import { useOrganizationsSystemsListQuery } from "@src/store/reducers/api";
 const SystemsView = () => {
-  const { isLoading } = useOrganizationsSystemsListQuery({ page: 1 });
+  const selectedOrganization = useAppSelector(
+    (state) => state.organization.selectedOrganization
+  );
+
+  const { isLoading } = useOrganizationsSystemsListQuery({
+    page: 1,
+    id: selectedOrganization?.id.toString(),
+  });
   return <>{!isLoading ? <SystemSection /> : <SectionSkeleton />}</>;
 };
 
