6ba3bdfd9aa27dc98742a2dd9941a79c4c3352d5
Umair Khan <umair.khan@toptal.com>
2022-01-11 12:32:54 +0500

    Filter on id

--- core/filters.py
+++ core/filters.py
@@ -4,7 +4,7 @@ from core import models
 
 
 class SystemFilters(filters.FilterSet):
-    health_network = filters.CharFilter(field_name="site__organization__name")
+    health_network = filters.CharFilter(field_name="site__organization")
 
     class Meta:
         model = models.System
