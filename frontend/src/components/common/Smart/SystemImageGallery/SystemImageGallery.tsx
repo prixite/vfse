75c97dd9f71e696dca80d9e2a1b57134a6c77f8d
Umair Khan <umair.khan@toptal.com>
2022-01-20 16:47:32 +0500

    Update image gallery

--- frontend/src/components/common/Smart/SystemImageGallery/SystemImageGallery.tsx
+++ frontend/src/components/common/Smart/SystemImageGallery/SystemImageGallery.tsx
@@ -1,6 +1,7 @@
-import { SetStateAction, useState, useRef, Dispatch, useEffect } from "react";
+import { SetStateAction, useState, Dispatch, useEffect } from "react";
 
-import Flicking from "@egjs/react-flicking";
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 import { useSystemsImagesListQuery } from "@src/store/reducers/api";
 
@@ -12,7 +13,6 @@ interface galleryProps {
 
 const SystemImageGallery = ({ setSystemImage }: galleryProps) => {
   const [index, setIndex] = useState(0);
-  const carouselRef = useRef(null);
   const { data, isFetching } = useSystemsImagesListQuery();
 
   useEffect(() => {
@@ -29,42 +29,30 @@ const SystemImageGallery = ({ setSystemImage }: galleryProps) => {
 
   return (
     <div className="systemGallery">
-      {/* <div className="selectedImage">
-               <img src={selectedImage}/>
-           </div> */}
       {!isFetching && data?.length ? (
-        <Flicking
-          ref={carouselRef}
-          tag="div"
-          viewportTag="div"
-          cameraTag="div"
-          classPrefix="eg-flick"
-          deceleration={0.0075}
-          zIndex={4000}
-          horizontal
-          bound
-          gap={15}
-        >
-          {data.map((image, imgIndex) => (
-            <div
-              className="image"
-              key={imgIndex}
-              onClick={() => handleSelectedImage(imgIndex, image?.id)}
-            >
-              <img
-                src={image.image}
-                style={{
-                  border: `${
-                    index === imgIndex ? "5px solid rgb(119, 60, 189)" : ""
-                  }`,
-                }}
-              />
-            </div>
-          ))}
-        </Flicking>
-      ) : (
-        <h3>loading</h3>
-      )}
+      <ImageList sx={{ width: '100%', height: 300 }} cols={4} rowHeight={150}>
+      {data.map((item, imgIndex) => (
+        <ImageListItem key={item.id} onClick={() => handleSelectedImage(imgIndex, item?.id)}>
+          <img
+            src={`${item.image}?w=160&h=160&fit=crop&auto=format`}
+            srcSet={`${item.image}?w=160&h=160&fit=crop&auto=format&dpr=2 2x`}
+            loading="lazy"
+            style={{
+              border: `${
+                index === imgIndex ? "5px solid rgb(119, 60, 189)" : ""
+              }`,
+              width: '164px',
+              height: '164px',
+              cursor: 'pointer',
+              marginBottom: '4px'
+            }}
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+    ) : (
+      <h3>loading</h3>
+    )} 
     </div>
   );
 };
