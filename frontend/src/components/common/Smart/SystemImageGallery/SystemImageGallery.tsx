dd061b9fc201ef8d8aaa2d74c2aac923688fbc03
Umair Khan <umair.khan@toptal.com>
2022-01-20 16:48:43 +0500

    Update image gallery

--- frontend/src/components/common/Smart/SystemImageGallery/SystemImageGallery.tsx
+++ frontend/src/components/common/Smart/SystemImageGallery/SystemImageGallery.tsx
@@ -1,7 +1,7 @@
 import { SetStateAction, useState, Dispatch, useEffect } from "react";
 
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 
 import { useSystemsImagesListQuery } from "@src/store/reducers/api";
 
@@ -30,29 +30,32 @@ const SystemImageGallery = ({ setSystemImage }: galleryProps) => {
   return (
     <div className="systemGallery">
       {!isFetching && data?.length ? (
-      <ImageList sx={{ width: '100%', height: 300 }} cols={4} rowHeight={150}>
-      {data.map((item, imgIndex) => (
-        <ImageListItem key={item.id} onClick={() => handleSelectedImage(imgIndex, item?.id)}>
-          <img
-            src={`${item.image}?w=160&h=160&fit=crop&auto=format`}
-            srcSet={`${item.image}?w=160&h=160&fit=crop&auto=format&dpr=2 2x`}
-            loading="lazy"
-            style={{
-              border: `${
-                index === imgIndex ? "5px solid rgb(119, 60, 189)" : ""
-              }`,
-              width: '164px',
-              height: '164px',
-              cursor: 'pointer',
-              marginBottom: '4px'
-            }}
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
-    ) : (
-      <h3>loading</h3>
-    )} 
+        <ImageList sx={{ width: "100%", height: 300 }} cols={4} rowHeight={150}>
+          {data.map((item, imgIndex) => (
+            <ImageListItem
+              key={item.id}
+              onClick={() => handleSelectedImage(imgIndex, item?.id)}
+            >
+              <img
+                src={`${item.image}?w=160&h=160&fit=crop&auto=format`}
+                srcSet={`${item.image}?w=160&h=160&fit=crop&auto=format&dpr=2 2x`}
+                loading="lazy"
+                style={{
+                  border: `${
+                    index === imgIndex ? "5px solid rgb(119, 60, 189)" : ""
+                  }`,
+                  width: "164px",
+                  height: "164px",
+                  cursor: "pointer",
+                  marginBottom: "4px",
+                }}
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      ) : (
+        <h3>loading</h3>
+      )}
     </div>
   );
 };
