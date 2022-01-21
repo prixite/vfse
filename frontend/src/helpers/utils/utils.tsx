ad83398f52469f1724e60e1833c846ec1da158d2
Umair Khan <umair.khan@toptal.com>
2022-01-21 18:07:11 +0500

    Fix add system issues

---
+++ frontend/src/helpers/utils/utils.tsx
@@ -0,0 +1,20 @@
+
+const ValidateIPaddress = (ipaddress: string)=> {  
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {  
+      return (true)  
+    }  
+    return (false)  
+} 
+
+const isValidURL = (url: string)=> {
+    var res = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (res !== null)
+}
+
+  
+
+
+export {
+    ValidateIPaddress,
+    isValidURL
+}
