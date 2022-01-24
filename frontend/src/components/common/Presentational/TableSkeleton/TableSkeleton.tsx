cd80044856bf2734967f72e50f93f621234cef51
Umair Khan <umair.khan@toptal.com>
2022-01-24 18:10:18 +0500

    Add table skeleton.

---
+++ frontend/src/components/common/Presentational/TableSkeleton/TableSkeleton.tsx
@@ -0,0 +1,27 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const TableSkeleton = () => {
+    return (
+      <>
+        <div style={{ marginTop : "55px" }}>
+            <Skeleton height={50} />
+            <div style={{ marginTop : "40px" }}>
+                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
+                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
+                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
+                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
+                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
+                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
+                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
+                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
+                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
+                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
+                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
+                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
+            </div>
+        </div>
+      </>
+    );
+  };
+  export default TableSkeleton;
--- frontend/src/views/documentation/DocumentationView.tsx
+++ frontend/src/views/documentation/DocumentationView.tsx
@@ -1,8 +1,9 @@
+import TableSkeleton from "@src/components/common/Presentational/TableSkeleton/TableSkeleton";
 import DocumentationSection from "@src/components/common/Smart/DocumentationSection/DocumentationSection";
 import { useProductsModelsListQuery } from "@src/store/reducers/api";
 
 export default function DocumentationView() {
   const { isLoading } = useProductsModelsListQuery();
 
-  return <>{!isLoading ? <DocumentationSection /> : <p>Loading...</p>}</>;
+  return <>{!isLoading ? <DocumentationSection /> : <TableSkeleton />}</>;
 }
