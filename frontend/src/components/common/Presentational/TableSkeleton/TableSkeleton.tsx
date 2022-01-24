cb9cd932b72267f095ba885c9b50e362cb15bbd1
Umair Khan <umair.khan@toptal.com>
2022-01-24 21:15:54 +0500

    Fix style issues;

--- frontend/src/components/common/Presentational/TableSkeleton/TableSkeleton.tsx
+++ frontend/src/components/common/Presentational/TableSkeleton/TableSkeleton.tsx
@@ -2,26 +2,26 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const TableSkeleton = () => {
-    return (
-      <>
-        <div style={{ marginTop : "55px" }}>
-            <Skeleton height={50} />
-            <div style={{ marginTop : "40px" }}>
-                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
-                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
-                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
-                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
-                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
-                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
-                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
-                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
-                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
-                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
-                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
-                <Skeleton height={30} style={{ marginBottom : "30px" }}/>
-            </div>
+  return (
+    <>
+      <div style={{ marginTop: "55px" }}>
+        <Skeleton height={50} />
+        <div style={{ marginTop: "40px" }}>
+          <Skeleton height={30} style={{ marginBottom: "30px" }} />
+          <Skeleton height={30} style={{ marginBottom: "30px" }} />
+          <Skeleton height={30} style={{ marginBottom: "30px" }} />
+          <Skeleton height={30} style={{ marginBottom: "30px" }} />
+          <Skeleton height={30} style={{ marginBottom: "30px" }} />
+          <Skeleton height={30} style={{ marginBottom: "30px" }} />
+          <Skeleton height={30} style={{ marginBottom: "30px" }} />
+          <Skeleton height={30} style={{ marginBottom: "30px" }} />
+          <Skeleton height={30} style={{ marginBottom: "30px" }} />
+          <Skeleton height={30} style={{ marginBottom: "30px" }} />
+          <Skeleton height={30} style={{ marginBottom: "30px" }} />
+          <Skeleton height={30} style={{ marginBottom: "30px" }} />
         </div>
-      </>
-    );
-  };
-  export default TableSkeleton;
+      </div>
+    </>
+  );
+};
+export default TableSkeleton;
