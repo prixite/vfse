ed3ce137b45aad5c2205a303c5b5f995b3292085
Umair Khan <umair.khan@toptal.com>
2022-01-27 11:01:22 +0500

    Do style fixies

--- frontend/src/views/vfse/VfseView.tsx
+++ frontend/src/views/vfse/VfseView.tsx
@@ -1,8 +1,7 @@
+import SystemCard from "@src/components/common/Presentational/SystemCard/SystemCard";
 import TableSkeleton from "@src/components/common/Presentational/TableSkeleton/TableSkeleton";
 import { useAppSelector } from "@src/store/hooks";
 import { useOrganizationsSeatsListQuery } from "@src/store/reducers/api";
-import SystemCard from "@src/components/common/Presentational/SystemCard/SystemCard";
-
 
 export default function VfseView() {
   const selectedOrganization = useAppSelector(
@@ -17,24 +16,26 @@ export default function VfseView() {
     return <TableSkeleton />;
   }
 
-  return  <>
-    {data?.map((item, key) => (
-      <SystemCard
-        key={key}
-        name={item.system.name}
-        image={item.system.image_url}
-        his_ris_info={item.system.his_ris_info}
-        dicom_info={item.system?.dicom_info}
-        serial_number={item.system?.serial_number}
-        asset_number={item.system?.asset_number}
-        mri_embedded_parameters={item.system.mri_embedded_parameters}
-        ip_address={item.system?.ip_address}
-        local_ae_title={item.system?.local_ae_title}
-        software_version={item.system?.software_version}
-        location_in_building={item.system?.location_in_building}
-        grafana_link={item.system?.grafana_link}
-        documentation={item.system?.documentation}
-      />
-    ))}
-  </>
+  return (
+    <>
+      {data?.map((item, key) => (
+        <SystemCard
+          key={key}
+          name={item.system.name}
+          image={item.system.image_url}
+          his_ris_info={item.system.his_ris_info}
+          dicom_info={item.system?.dicom_info}
+          serial_number={item.system?.serial_number}
+          asset_number={item.system?.asset_number}
+          mri_embedded_parameters={item.system.mri_embedded_parameters}
+          ip_address={item.system?.ip_address}
+          local_ae_title={item.system?.local_ae_title}
+          software_version={item.system?.software_version}
+          location_in_building={item.system?.location_in_building}
+          grafana_link={item.system?.grafana_link}
+          documentation={item.system?.documentation}
+        />
+      ))}
+    </>
+  );
 }
