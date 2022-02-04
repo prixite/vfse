45693c8774e46bdeb9ca6046fb1837ec35995729
Umair Khan <umair.khan@toptal.com>
2022-02-04 18:18:34 +0500

    Remove print

--- core/tests/test_roles.py
+++ core/tests/test_roles.py
@@ -15,37 +15,29 @@ class PermissionTestCase(BaseTestCase):
             if str(path.pattern).startswith("api/")
         ]
 
-        print("Checking GET requests,", end=" ")
         for url in paths:
             url_path = re.sub(r"<([\w:]+)>", "1", str(url))
             response = self.client.get(f"/{url_path}")
             self.assertNotEqual(response.status_code, 403)
-        print("completed successfully.")
 
-        print("Checking POST requests,", end=" ")
         for url in paths:
             url_path = re.sub(r"<([\w:]+)>", "1", str(url))
             response = self.client.post(f"/{url_path}", data={})
             self.assertNotEqual(response.status_code, 403)
-        print("completed successfully.")
 
-        print("Checking PATCH requests,", end=" ")
         for url in paths:
             url_path = re.sub(r"<([\w:]+)>", "1", str(url))
             response = self.client.patch(
                 f"/{url_path}", data={"users": [], "health_networks": [], "email": ""}
             )
             self.assertNotEqual(response.status_code, 403)
-        print("completed successfully.")
 
-        print("Checking PUT requests,", end=" ")
         for url in paths:
             url_path = re.sub(r"<([\w:]+)>", "1", str(url))
             response = self.client.put(
                 f"/{url_path}", data={"users": [], "health_networks": []}
             )
             self.assertNotEqual(response.status_code, 403)
-        print("completed successfully.")
 
     def test_customer_admin_role(self):
         pass
