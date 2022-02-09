237f2c8bcad9816f1588dc9f56fa2a38ae830dd2
Umair Khan <umair.khan@toptal.com>
2022-02-09 19:42:48 +0500

    Add /api/organizations/ test

--- core/tests/test_permissions.py
+++ core/tests/test_permissions.py
@@ -19,3 +19,13 @@ class UnrestrictedURLsTestCase(BaseTestCase):
                 with self.subTest(user=user, url=url):
                     self.client.force_login(user)
                     assert self.client.get(url).status_code == 200
+
+
+class OrganizationTestCase(BaseTestCase):
+    def test_get_super(self):
+        for user in [self.super_admin, self.super_manager]:
+            with self.subTest(user=user):
+                self.client.force_login(user)
+                response = self.client.get("/api/organizations/")
+                assert response.status_code == 200
+                assert len(response.json()) == 3
