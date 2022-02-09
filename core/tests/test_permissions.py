3f362546d8885a09074405af405bf62380e49b19
Umair Khan <umair.khan@toptal.com>
2022-02-09 19:40:30 +0500

    Fix test

--- core/tests/test_permissions.py
+++ core/tests/test_permissions.py
@@ -12,8 +12,8 @@ class UnrestrictedURLsTestCase(BaseTestCase):
             "/api/modalities/",
             "/api/products/",
             "/api/products/models/",
-            "api/manufacturers/",
-            "api/manufacturers/images/",
+            "/api/manufacturers/",
+            "/api/manufacturers/images/",
         ]:
             for user in models.User.objects.filter(is_lambda_user=False):
                 with self.subTest(user=user, url=url):
