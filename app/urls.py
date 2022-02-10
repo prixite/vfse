adf3be41878c87b3c8f50b0ac9958f2534785fb1
Umair Khan <umair.khan@toptal.com>
2022-02-10 16:18:07 +0500

    Revert urls.py

--- app/urls.py
+++ app/urls.py
@@ -69,11 +69,7 @@ api_urlpatterns = [
     ),
     path(
         "api/lambda/",
-        api.LambdaView.as_view(
-            {
-                "patch": "partial_update",
-            }
-        ),
+        api.LambdaView.as_view({"patch": "partial_update"}),
     ),
     path(
         "api/organizations/<int:pk>/health_networks/",
