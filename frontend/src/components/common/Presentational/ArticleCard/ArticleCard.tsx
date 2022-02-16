e78c066b2931edd5f9b062dda90390f02c67f404
Umair Khan <umair.khan@toptal.com>
2022-02-16 17:20:10 +0500

    Add another card

---
+++ frontend/src/components/common/Presentational/ArticleCard/ArticleCard.tsx
@@ -0,0 +1,38 @@
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import { Box } from "@mui/material";
+import InsertDriveFileOutlinedIcon from '@mui/icons-material/InsertDriveFileOutlined';
+import { hexToRgb } from "@src/helpers/utils/utils";
+import TextsmsOutlinedIcon from '@mui/icons-material/TextsmsOutlined';
+import "@src/components/common/Presentational/ArticleCard/ArticleCard.scss";
+import { textChangeRangeIsUnchanged } from "typescript";
+interface props {
+  color: string;
+  number: number;
+}
+
+const ArticleCard = ({ color, number }: props) => {
+  return (
+    <div className="Article-card">
+      <Box component="div" className="card">
+        <div className="circle" style={{ background: `${hexToRgb(color, 0.3)}` }}>
+          <InsertDriveFileOutlinedIcon style={{ color: color }}/>
+        </div>
+        <div className="heading">
+          <h2 className="title" style={{ color: color }}> Getting started</h2>
+        </div>
+        <div className="article-no">
+         <TextsmsOutlinedIcon style={{color: '#696F77', fontSize: '1em', marginRight: '10px'}}/> 
+          <p>8 articles</p>
+          </div>
+        <div className="explore">
+          <p className="text" style={{ color: color }}>
+            Explore
+          </p>
+          <ArrowRightAltIcon style={{ color: color }} />
+        </div>
+      </Box>
+    </div>
+  );
+};
+
+export default ArticleCard;
--- frontend/src/components/common/Smart/KnowledgeSection/KnowledgeSection.tsx
+++ frontend/src/components/common/Smart/KnowledgeSection/KnowledgeSection.tsx
@@ -2,6 +2,7 @@ import { Grid, Box } from "@mui/material";
 
 import KnowledgeTopCard from "@src/components/common/Presentational/KnowledgeTopCard/KnowledgeTopCard";
 import TopViewBtns from "@src/components/common/Smart/TopViewBtns/TopViewBtns";
+import ArticleCard from "../../Presentational/ArticleCard/ArticleCard";
 import "@src/components/common/Smart/KnowledgeSection/KnowledgeSection.scss";
 
 const topData = [
@@ -29,10 +30,17 @@ const KnowledgeSection = () => {
       <h1 className="main-heading">Knowledge base</h1>
       <TopViewBtns path="documentation" />
       <h2 className="sub-heading">Top Help Articles</h2>
-      <Grid container spacing={2}>
+      <Grid container spacing={1}>
         {topData.map((item, index) => (
           <Grid item={true} xs={3} key={index}>
             <KnowledgeTopCard color={item?.color} number={item?.number} />
+          </Grid>
+        ))}
+      </Grid>
+      <Grid container spacing={2}>
+        {topData.map((item, index) => (
+          <Grid item={true} xs={3} key={index} style={{marginTop: '12px'}}>
+            <ArticleCard color={item?.color} number={item?.number} />
           </Grid>
         ))}
       </Grid>
