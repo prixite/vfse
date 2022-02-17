02909acf1ae5604780b01738afc7e72003ef64d6
Umair Khan <umair.khan@toptal.com>
2022-02-17 13:56:00 +0500

    Formatting

--- vfse/admin.py
+++ vfse/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+
 from vfse import models
+
 
 @admin.register(models.Folder)
 class FolerAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
