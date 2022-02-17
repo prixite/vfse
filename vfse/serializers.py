65098716898fd080eb9078acb131abcb91fb4a12
Umair Khan <umair.khan@toptal.com>
2022-02-17 15:34:59 +0500

    Formatting

--- vfse/serializers.py
+++ vfse/serializers.py
@@ -1,16 +1,21 @@
 from rest_framework import serializers
+
 from vfse import models
+
 
 class FolderSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Folder
-        fields = ['id','name','category']
+        fields = ["id", "name", "category"]
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Category
-        fields = ['id','name']
+        fields = ["id", "name"]
+
+
 class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Document
-        fields = ['id','text','created_by']
+        fields = ["id", "text", "created_by"]
