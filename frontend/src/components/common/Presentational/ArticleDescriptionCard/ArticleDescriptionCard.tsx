f5da45befed4896e9093f79267d47d1df4f7795f
Umair Khan <umair.khan@toptal.com>
2022-02-25 13:53:51 +0500

    Fix cards

--- frontend/src/components/common/Presentational/ArticleDescriptionCard/ArticleDescriptionCard.tsx
+++ frontend/src/components/common/Presentational/ArticleDescriptionCard/ArticleDescriptionCard.tsx
@@ -1,3 +1,5 @@
+import { localizedData } from "@src/helpers/utils/language";
+import { LocalizationInterface } from "@src/helpers/interfaces/localizationinterfaces";
 import "@src/components/common/Presentational/ArticleDescriptionCard/ArticleDescriptionCard.scss";
 
 interface ArticleDescription {
@@ -11,18 +13,20 @@ const ArticleDescriptionCard = ({
   description,
   startGuide,
 }: ArticleDescription) => {
+  const localization : LocalizationInterface = localizedData();
+  const { title1, title2, title3 } = localization.articleDescription;
   return (
     <div className="article-description">
       <h1 className="heading" style={{ marginBottom: "18px" }}>
-        Overview
+        {title1}
       </h1>
       <p className="description">{overview}</p>
       <h1 className="heading" style={{ margin: "18px 0" }}>
-        Getting started
+        {title2}
       </h1>
       <p className="description">{description}</p>
       <h1 className="heading" style={{ margin: "18px 0" }}>
-        Start guide
+        {title3}
       </h1>
       <p className="description">{startGuide}</p>
     </div>
