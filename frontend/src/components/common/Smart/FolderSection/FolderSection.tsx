b250dad31cacd4fe778d6258ac814c193167b0b7
Umair Khan <umair.khan@toptal.com>
2022-03-02 14:35:46 +0500

    Add search functionality on document page

--- frontend/src/components/common/Smart/FolderSection/FolderSection.tsx
+++ frontend/src/components/common/Smart/FolderSection/FolderSection.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
+
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 import { Grid, Box } from "@mui/material";
 import { Link, useParams } from "react-router-dom";
-import NoDataFound from "@src/components/shared/NoDataFound/NoDataFound";
+
 import KnowledgeTopCard from "@src/components/common/Presentational/KnowledgeTopCard/KnowledgeTopCard";
 import TopViewBtns from "@src/components/common/Smart/TopViewBtns/TopViewBtns";
+import NoDataFound from "@src/components/shared/NoDataFound/NoDataFound";
 import { LocalizationInterface } from "@src/helpers/interfaces/localizationinterfaces";
 import { constants } from "@src/helpers/utils/constants";
 import { localizedData } from "@src/helpers/utils/language";
@@ -47,29 +49,28 @@ const topData = {
 };
 
 interface document {
-   query: string; 
-   results?: { name: string }[] 
+  query: string;
+  results?: { name: string }[];
 }
 
 const FolderSection = () => {
   const { id } = useParams();
-  const doc : document = {results : [], query: ''};
+  const doc: document = { results: [], query: "" };
   const [documentList, setDocumentList] = useState(doc);
   const [list, setList] = useState([]);
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const { noDataTitle, noDataDescription } = localizedData().systems;
   const localization: LocalizationInterface = localizedData();
   const { backBtn } = localization.folderSection;
   const { organizationRoute } = constants;
 
-  useEffect(()=>{
-      if(query?.length > 2){
-        setList(documentList?.results);
-      }
-      else{
-        setList(topData?.results);
-      }
-  },[topData, documentList, query, setList])
+  useEffect(() => {
+    if (query?.length > 2) {
+      setList(documentList?.results);
+    } else {
+      setList(topData?.results);
+    }
+  }, [topData, documentList, query, setList]);
 
   return (
     <Box component="div" className="folder-heading">
@@ -86,11 +87,16 @@ const FolderSection = () => {
         </div>
       </Link>
       <h2 className="main-heading">{topData?.folderName}</h2>
-      <TopViewBtns path="description" actualData={topData?.results} searchText={query} setSearchText={setQuery} setList={setDocumentList}/>
+      <TopViewBtns
+        path="description"
+        actualData={topData?.results}
+        searchText={query}
+        setSearchText={setQuery}
+        setList={setDocumentList}
+      />
       <Grid container spacing={1} style={{ marginTop: "30px" }}>
-        {
-          query?.length > 2 && documentList.query === query ? 
-            list.map((item, index) => (
+        {query?.length > 2 && documentList.query === query
+          ? list.map((item, index) => (
               <Grid item={true} xs={6} xl={2} md={3} key={index}>
                 <KnowledgeTopCard
                   title={item?.name}
@@ -98,9 +104,8 @@ const FolderSection = () => {
                 />
               </Grid>
             ))
-          :
-          query?.length < 2 || !query?.length ?
-            list.map((item, index) => (
+          : query?.length < 2 || !query?.length
+          ? list.map((item, index) => (
               <Grid item={true} xs={6} xl={2} md={3} key={index}>
                 <KnowledgeTopCard
                   title={item?.name}
@@ -108,33 +113,32 @@ const FolderSection = () => {
                 />
               </Grid>
             ))
-            :''
-      }
+          : ""}
       </Grid>
-      {
-        query?.length > 2 && documentList.query !== query? 
+      {query?.length > 2 && documentList.query !== query ? (
         <div
-        style={{
-          color: "gray",
-          marginLeft: "45%",
-          marginTop: "20%",
-        }}
-      >
-        <h2>{ "Loading...."}</h2>
-      </div>
-          : ''
-      }
-      {
-        query?.length > 2 && !list?.length && documentList.query === query? 
+          style={{
+            color: "gray",
+            marginLeft: "45%",
+            marginTop: "20%",
+          }}
+        >
+          <h2>{"Loading...."}</h2>
+        </div>
+      ) : (
+        ""
+      )}
+      {query?.length > 2 && !list?.length && documentList.query === query ? (
         <NoDataFound
-              search
-              setQuery={setQuery}
-              queryText={query}
-              title={noDataTitle}
-              description={noDataDescription}
-          />
-          : ''
-      }
+          search
+          setQuery={setQuery}
+          queryText={query}
+          title={noDataTitle}
+          description={noDataDescription}
+        />
+      ) : (
+        ""
+      )}
     </Box>
   );
 };
