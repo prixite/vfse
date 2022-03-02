2cfe8957dcc5abf93eb3a7c565af7c179444ff27
Umair Khan <umair.khan@toptal.com>
2022-03-02 12:57:51 +0500

    Formatting

--- core/views/api.py
+++ core/views/api.py
@@ -324,12 +324,14 @@ class SystemViewSet(OrganizationSystemViewSet):
                 query = f"""from(bucket: "{INFLUX_BUCKET}")
                 |> range(start: -7d)
                 |> filter(fn:(r) => r._measurement=="HeLevel" and r._field=="value")
-        |> filter(fn:(r) => r["IPAddress"]=="{ip_address}" and r["CityName"]=="{city_name}")
+    |> filter(fn:(r) => r["IPAddress"]=="{ip_address}" and r["CityName"]=="{city_name}")
         |> sort(columns: ["_time"], desc: false)
                 |> last()
-                """
+                """  # noqa
                 tables = client.query_api().query(query, org=INFLUX_ORG)
-                system.mri_embedded_parameters["helium"] = tables[0].records[0]["_value"]
+                system.mri_embedded_parameters["helium"] = tables[0].records[0][
+                    "_value"
+                ]
                 system.save()
                 client.close()
         except IndexError:
