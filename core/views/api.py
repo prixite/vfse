60c2d27cbebe0a9e70dbbf47d83ce8369d915490
Umair Khan <umair.khan@toptal.com>
2022-03-02 12:49:58 +0500

    MInor fixes

--- core/views/api.py
+++ core/views/api.py
@@ -313,23 +313,29 @@ class SystemViewSet(OrganizationSystemViewSet):
     def retrieve_from_influx(self, request, *args, **kwargs):
         ip_address = "192.168.1.1"
         city_name = "Plantation"
-        with InfluxDBClient(
-            url=INFLUX_DB_URL,
-            token=INFLUX_TOKEN,
-            org=INFLUX_ORG,
-        ) as client:
-            query = f"""from(bucket: "{INFLUX_BUCKET}")
-            |> range(start: -1h)
-            |> filter(fn:(r) => r._measurement=="HeLevel" and r._field=="value")
-    |> filter(fn:(r) => r["IPAddress"]=="{ip_address}" and r["CityName"]=="{city_name}")
-            |> mean()
-            """
-            tables = client.query_api().query(query, org=INFLUX_ORG)
-            system = models.System.objects.get(id=kwargs["system_pk"])
-            system.mri_embedded_parameters["helium"] = tables[0].records[0]["_value"]
-            system.save()
-
-            client.close()
+        system = models.System.objects.get(id=kwargs["system_pk"])
+        tables = None
+        try:
+            with InfluxDBClient(
+                url=INFLUX_DB_URL,
+                token=INFLUX_TOKEN,
+                org=INFLUX_ORG,
+            ) as client:
+                query = f"""from(bucket: "{INFLUX_BUCKET}")
+                |> range(start: -7d)
+                |> filter(fn:(r) => r._measurement=="HeLevel" and r._field=="value")
+        |> filter(fn:(r) => r["IPAddress"]=="{ip_address}" and r["CityName"]=="{city_name}")
+        |> sort(columns: ["_time"], desc: false)
+                |> last()
+                """
+                tables = client.query_api().query(query, org=INFLUX_ORG)
+                system.mri_embedded_parameters["helium"] = tables[0].records[0]["_value"]
+                system.save()
+                client.close()
+        except IndexError:
+            print("No records found")
+        except Exception as e:
+            print(e)
 
 
 class UserViewSet(ModelViewSet, mixins.UserMixin):
