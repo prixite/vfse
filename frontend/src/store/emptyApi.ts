48d50342ba04c60643f79ab8bd7294e98b7156bd
Umair Khan <umair.khan@toptal.com>
2022-04-07 11:25:35 +0500

    Use @src

--- frontend/src/store/emptyApi.ts
+++ frontend/src/store/emptyApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-import { Category, Document, Folder } from "./reducers/generated";
+import { Category, Document, Folder } from "@src/store/reducers/generated";
 
 // initialize an empty api service that we'll inject endpoints into later as needed
 export const emptySplitApi = createApi({
