478e854e43ba515b38863416b8f964f49aad9eb7
Waqar Ali <waqar.ali@prixite.com>
2022-04-19 07:41:17 +0000

    SSH Service Backend

---
+++ webssh/__init__.py
@@ -0,0 +1,14 @@
+import sys
+
+from webssh._version import __version__, __version_info__  # noqa
+
+__author__ = "Shengdun Hua <webmaster0115@gmail.com>"
+
+if (
+    sys.platform == "win32"
+    and sys.version_info.major == 3
+    and sys.version_info.minor >= 8
+):
+    import asyncio
+
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
