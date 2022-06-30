9f6f38fcf9a5adad5c3330b9f7476cc7c5e03965
Muhammad Shafiq <muhammad.shafiq@evampsaanga.com>
2022-06-30 14:05:17 +0500

    Completed top section of forum section responsiveness/empty spaces fixes

--- frontend/src/components/common/smart/vfseTopSection/VfseTopSection.tsx
+++ frontend/src/components/common/smart/vfseTopSection/VfseTopSection.tsx
@@ -1,4 +1,3 @@
-// import CountingInfoCards from "@src/components/common/Presentational/CountingInfoCards/CountingInfoCards";
 import React, { useState, useMemo, useEffect } from "react";
 
 import AddIcon from "@mui/icons-material/Add";
@@ -11,7 +10,6 @@ import Select from "@mui/material/Select";
 import debouce from "lodash.debounce";
 
 import TopicUpdatesCards from "@src/components/common/presentational/topicUpdatesCards/TopicUpdatesCard";
-import useStyles from "@src/components/common/smart/vfseTopSection//Styles";
 import { localizedData } from "@src/helpers/utils/language";
 import { useAppSelector } from "@src/store/hooks";
 import { api, VfseTopicsListApiResponse } from "@src/store/reducers/api";
@@ -33,7 +31,6 @@ export default function VfseTopSection({
   paginatedTopics,
   setPaginatedTopics,
 }: Props) {
-  const classes = useStyles();
   const { data: popularTopicData = [] } = api.useGetPopularTopicsQuery(); //popular
   const { data: topicsList = [] } = api.useGetTopicsListQuery({});
 
@@ -255,15 +252,9 @@ export default function VfseTopSection({
 
   return (
     <>
-      <Grid
-        container
-        direction="row"
-        justifyContent="flex-start"
-        alignItems="center"
-      >
-        {/* Sort  */}
-        <Grid item xs={4} sm={4} md={2} lg={2}>
-          <FormControl className={classes.sortByField} size="small">
+      <Grid container spacing={2} mt={3}>
+        <Grid item xs={6} md={2} lg={2}>
+          <FormControl fullWidth size="small" sx={{ backgroundColor: "#fff" }}>
             <InputLabel id="sort-by-categoty">Sort By:</InputLabel>
             <Select
               labelId="sort-by-category-label"
@@ -283,13 +274,8 @@ export default function VfseTopSection({
           </FormControl>
         </Grid>
 
-        {/* Filter  */}
-        <Grid item xs={2} sm={2} md={1} lg={2} ml={3}>
-          <FormControl
-            // sx={{ m: 0, mr: 1, minWidth: 140, backgroundColor: "white" }}
-            className={classes.filterField}
-            size="small"
-          >
+        <Grid item xs={6} md={2} lg={2}>
+          <FormControl sx={{ backgroundColor: "#fff" }} fullWidth size="small">
             <InputLabel id="filter-dropdown">Filter:</InputLabel>
             <Select
               labelId="filter-dropdown-label"
@@ -308,16 +294,15 @@ export default function VfseTopSection({
             </Select>
           </FormControl>
         </Grid>
-
-        {/* Search  */}
-        <Grid item xs={12} sm={12} md={2} lg={3}>
+        <Grid item xs={12} md={2} lg={2}>
           <TextField
             id="search-topics"
             size="small"
-            className={classes.topSearchInput}
+            fullWidth
             variant="outlined"
             onChange={debouncedResults}
             placeholder="Search"
+            sx={{ backgroundColor: "#fff" }}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -327,10 +312,14 @@ export default function VfseTopSection({
             }}
           />
         </Grid>
-
-        {/* Create Topic Button  */}
-        <Grid item xs={6} sm={12} md={12} lg={4} className={classes.topBtnGrid}>
-          <Box className={classes.topBtnCreate}>
+        <Grid item xs={12} md={6} lg={6}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "center",
+            }}
+          >
             <Button
               variant="contained"
               size="large"
