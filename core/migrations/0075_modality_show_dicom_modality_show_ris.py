14d2b7f395d6582e12c2ddb675a07fe7f4ea6d0b
Umair Khan <umair.khan@toptal.com>
2022-07-04 11:59:58 +0500

    Run pre-commit

--- core/migrations/0075_modality_show_dicom_modality_show_ris.py
+++ core/migrations/0075_modality_show_dicom_modality_show_ris.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0074_system_ssh_user'),
+        ("core", "0074_system_ssh_user"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='modality',
-            name='show_dicom',
+            model_name="modality",
+            name="show_dicom",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='modality',
-            name='show_ris',
+            model_name="modality",
+            name="show_ris",
             field=models.BooleanField(default=False),
         ),
     ]
