7f804e1fd2ac1b9e94d44515284470ed387b8aba
getalihassan <ali.hassan@prixite.com>
2022-07-19 13:11:56 +0500

    feature pwa complete

--- frontend/webpack/webpack.config.js
+++ frontend/webpack/webpack.config.js
@@ -3,8 +3,6 @@
 const path = require("path");
 
 const { EnvironmentPlugin } = require("webpack");
-const WorkboxPlugin = require('workbox-webpack-plugin');
-
 require("dotenv").config();
 
 module.exports = {
@@ -43,14 +41,7 @@ module.exports = {
       REQUEST_TOKEN: "fake",
       WEBSSH_SERVER: "http://localhost:8888/",
       WEBSSH_WS: "ws://localhost:8888/",
-    }),
-    new WorkboxPlugin.GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
-      clientsClaim: true,
-      skipWaiting: true,
-      swDest: path.resolve("core/static/sw")
-    }),
+    })
   ],
   resolve: {
     alias: {
--- frontend/webpack/webpack.prod.config.js
+++ frontend/webpack/webpack.prod.config.js
@@ -1,12 +1,23 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 const merge = require("webpack-merge");
-
+const path = require("path");
 const common = require("./webpack.config.js");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge.mergeWithCustomize({
   customizeArray: merge.customizeArray({
     plugins: "prepend",
   }),
 })(common, {
+  plugins: [
+    ...common.plugins,
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+      swDest: path.resolve("frontend/src/serviceworker/sw"),
+    }),
+  ],
   mode: "production",
 });
