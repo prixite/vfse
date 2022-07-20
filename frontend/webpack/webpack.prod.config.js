52929af5fe1262776fa98f49e26a7072222fb7bd
Ali Hassan <94823010+getalihassan@users.noreply.github.com>
2022-07-20 16:45:23 +0500

    Draft: Feature/pwa (#163)
    * pwa in progress
    
    * pwa in progress
    
    * feature pwa complete
    
    * feature pwa complete
    
    * feature pwa complete
    
    * feature pwa complete
    
    * feature pwa complete
    
    * feature pwa complete
    
    * Update frontend/src/serviceworker/manifest.json
    
    Co-authored-by: Umair Khan <umair.khan@toptal.com>
    
    * Update core/templates/base.html
    
    Co-authored-by: Umair Khan <umair.khan@toptal.com>
    
    * Update core/templates/base.html
    
    Co-authored-by: Umair Khan <umair.khan@toptal.com>
    
    * Update core/templates/base.html
    
    Co-authored-by: Umair Khan <umair.khan@toptal.com>
    
    * Update core/templates/base.html
    
    Co-authored-by: Umair Khan <umair.khan@toptal.com>
    
    * Update frontend/src/serviceworker/manifest.json
    
    Co-authored-by: Umair Khan <umair.khan@toptal.com>
    
    * Update frontend/src/serviceworker/manifest.json
    
    Co-authored-by: Umair Khan <umair.khan@toptal.com>
    
    * resolve comments
    
    * resolve comments
    
    * resolve comments
    
    Co-authored-by: Umair Khan <umair.khan@toptal.com>

--- frontend/webpack/webpack.prod.config.js
+++ frontend/webpack/webpack.prod.config.js
@@ -1,5 +1,8 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
+const path = require("path");
+
 const merge = require("webpack-merge");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 const common = require("./webpack.config.js");
 
@@ -8,5 +11,15 @@ module.exports = merge.mergeWithCustomize({
     plugins: "prepend",
   }),
 })(common, {
+  plugins: [
+    ...common.plugins,
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+      swDest: path.resolve("frontend/dist/web/sw"),
+    }),
+  ],
   mode: "production",
 });
