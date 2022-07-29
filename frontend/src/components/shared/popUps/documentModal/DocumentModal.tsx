5a3514f62fb91e5dcdb86222e529526afb119bde
Rabeel Qaiser <rabeel.qaiser@prixite.com>
2022-07-29 15:20:26 +0500

    Save instead of btnSave

--- frontend/src/components/shared/popUps/documentModal/DocumentModal.tsx
+++ frontend/src/components/shared/popUps/documentModal/DocumentModal.tsx
@@ -424,7 +424,7 @@ export default function DocumentModal({
             formik.handleSubmit();
           }}
         >
-          {action === "add" ? btnSave : btnSave}
+          {action === "add" ? btnSave : "Save"}
         </Button>
       </DialogActions>
     </Dialog>
