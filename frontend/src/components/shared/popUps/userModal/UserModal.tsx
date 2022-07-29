0cec5849d3040e8fa3220ddb050ddbde033d0d84
getalihassan <ali.hassan@prixite.com>
2022-07-29 14:04:01 +0500

    Revert "Revert "Revert "Revert "update user bugfix""""
    This reverts commit 51f1f79a619326c99210b852970c8f14dd4d227f.

--- frontend/src/components/shared/popUps/userModal/UserModal.tsx
+++ frontend/src/components/shared/popUps/userModal/UserModal.tsx
@@ -406,11 +406,19 @@ export default function UserModal(props: Props) {
           setIsLoading(false);
         }, 500);
       })
-      .catch(() => {
-        toast.error("User with this username already exists.", {
-          autoClose: 2000,
-          pauseOnHover: false,
-        });
+      .catch((error) => {
+        const metaError = error.data.meta
+          ? Object.keys(error.data.meta)[0] +
+            ": " +
+            error.data.meta[Object.keys(error.data.meta)[0]][0]
+          : null;
+        toast.error(
+          JSON.stringify(metaError || error.data.non_field_errors[0]),
+          {
+            autoClose: 2000,
+            pauseOnHover: false,
+          }
+        );
         setIsLoading(false);
       });
   };
