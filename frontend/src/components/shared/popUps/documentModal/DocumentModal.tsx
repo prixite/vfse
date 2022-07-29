455bd3f123e57218c250bd7296d8f3d604c3923d
Rabeel Qaiser <rabeel.qaiser@prixite.com>
2022-07-29 15:12:08 +0500

    Pre-Commit

--- frontend/src/components/shared/popUps/documentModal/DocumentModal.tsx
+++ frontend/src/components/shared/popUps/documentModal/DocumentModal.tsx
@@ -424,7 +424,7 @@ export default function DocumentModal({
             formik.handleSubmit();
           }}
         >
-          {action === "add" ? btnSave : btnSave }
+          {action === "add" ? btnSave : btnSave}
         </Button>
       </DialogActions>
     </Dialog>
