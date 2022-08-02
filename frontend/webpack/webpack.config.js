4b77035a76f3e47af624879fad3a65e7342ea486
getalihassan <ali.hassan@prixite.com>
2022-08-02 20:36:12 +0500

    replace with contentHash

--- frontend/webpack/webpack.config.js
+++ frontend/webpack/webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "../dist/"),
     publicPath: "/static/",
-    filename: "[name].[chunkhash].js",
+    filename: "[name].[contenthash].js",
     clean: true,
   },
 };
