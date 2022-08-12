5e9954b67c21abd1c78e45b9ab3eb2cd2710d541
Rabeel7 <107916405+Rabeel7@users.noreply.github.com>
2022-08-12 17:40:06 +0500

    BackBtn Style Fix (#428)

--- frontend/src/components/common/presentational/backBtn/BackBtn.tsx
+++ frontend/src/components/common/presentational/backBtn/BackBtn.tsx
@@ -4,11 +4,15 @@ import "@src/components/common/presentational/backBtn/backBtn.scss";
 const BackBtn = () => {
   const navigate = useNavigate();
   return (
-    <div className="back-btn" onClick={() => navigate(-1)}>
-      <ArrowRightAltIcon
-        style={{ transform: "rotate(180deg)", color: "rgb(0, 0, 0)" }}
-      />
-      <p className="back-text">Back</p>
+    <div className="back-btn">
+      <div className="back-btn-container">
+        <ArrowRightAltIcon
+          style={{ transform: "rotate(180deg)", color: "rgb(0, 0, 0)" }}
+        />
+        <p className="back-text" onClick={() => navigate(-1)}>
+          Back
+        </p>
+      </div>
     </div>
   );
 };
