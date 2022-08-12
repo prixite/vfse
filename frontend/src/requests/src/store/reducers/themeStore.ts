c3d60a8b4ef4009108d8da1289972bdf9cdd1325
aslamayesha <38494564+aslamayesha@users.noreply.github.com>
2022-08-12 15:27:07 +0500

    Fix reuest page error (#281)
    * Fix reuest page error
    
    RT #242
    
    * Change import name
    
    RT #242
    
    * Fix request error.
    
    RT #242
    
    * Fix request error.
    
    RT #242
    
    * Fix bug
    
    * Run pre-commit
    
    Co-authored-by: Ayesha <ayesha.aslam@prixite.com>
    Co-authored-by: Umair Khan <umair.khan@toptal.com>

---
+++ frontend/src/requests/src/store/reducers/themeStore.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface ThemeColorState {
+  sideBarBackground: string;
+  buttonBackground: string;
+  sideBarTextColor: string;
+  buttonTextColor: string;
+  secondaryColor: string;
+  fontOne: string;
+  fontTwo: string;
+}
+
+const initialState: ThemeColorState = {
+  sideBarBackground: "#2a3242",
+  buttonBackground: "#773cbe",
+  sideBarTextColor: "#94989E",
+  buttonTextColor: "#FFFFFF",
+  secondaryColor: "#EFE1FF",
+  fontOne: "helvetica",
+  fontTwo: "calibri",
+};
+
+export const themeSlice = createSlice({
+  name: "themeColor",
+  initialState,
+  reducers: {},
+});
+
+export default themeSlice.reducer;
