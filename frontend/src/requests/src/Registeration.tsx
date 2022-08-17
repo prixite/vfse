8f75a3cef3dcaabe002f24b246303396f80dfd0c
Ayesha <ayesha.aslam@prixite.com>
2022-08-17 17:50:45 +0500

    Fix navigation issue.
    RT #447

--- frontend/src/requests/src/Registeration.tsx
+++ frontend/src/requests/src/Registeration.tsx
@@ -383,17 +383,27 @@ const Registeration = () => {
           )}
           <>
             {page === 1 ? (
-              <Box component="div" className="btn-group" style={{alignItems: "flex-end"}}>
-              <Button className="register-btn" onClick={moveToNextPage}>
-                Next
-              </Button>
-              <div>
-                <span>Already have an account?</span>
-                  <Button variant="text" className="login-btn" onClick={()=> window.location.replace("/accounts/login/?next=/")}>
+              <Box
+                component="div"
+                className="btn-group"
+                style={{ alignItems: "flex-end" }}
+              >
+                <Button className="register-btn" onClick={moveToNextPage}>
+                  Next
+                </Button>
+                <div>
+                  <span>Already have an account?</span>
+                  <Button
+                    variant="text"
+                    className="login-btn"
+                    onClick={() =>
+                      window.location.replace("/accounts/login/?next=/")
+                    }
+                  >
                     log in
                   </Button>
-              </div>
-            </Box>
+                </div>
+              </Box>
             ) : (
               <Box component="div" className="btn-group">
                 <Button className="register-btn" onClick={() => setPage(1)}>
