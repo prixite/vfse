b2c6caf8b0c93429e80a9a085d1d0fc8cf07edbf
Saad Ashraf <saad.ashraf@prixite.com>
2022-09-12 15:01:12 +0500

    remove if condition.

--- core/permissions.py
+++ core/permissions.py
@@ -112,10 +112,11 @@ class OrganizationPermission(BasePermission):
 
 class OrganizationIsAdminPermission(BasePermission):
     def has_permission(self, request, view):
-        if request.method.lower() in ["post", "patch", "delete"]:
-            org_id = view.kwargs.get("pk", request.user.get_default_organization().id)
-            return not (models.Role.END_USER == request.user.get_organization_role(org_id) and request.method not in SAFE_METHODS)
-        return True
+        org_id = view.kwargs.get("pk", request.user.get_default_organization().id)
+        return not (
+            models.Role.END_USER == request.user.get_organization_role(org_id)
+            and request.method not in SAFE_METHODS
+        )
 
 
 class SystemNotePermissions(BasePermission):
