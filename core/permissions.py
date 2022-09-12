ff32b1a7211e0c2182d930d1fc9f7d7724dd78c8
Saad Ashraf <saad.ashraf@prixite.com>
2022-09-12 14:35:12 +0500

    update logic.

--- core/permissions.py
+++ core/permissions.py
@@ -113,11 +113,8 @@ class OrganizationPermission(BasePermission):
 class OrganizationIsAdminPermission(BasePermission):
     def has_permission(self, request, view):
         if request.method.lower() in ["post", "patch", "delete"]:
-            if request.user.is_superuser:
-                return True
-
-            return False
-
+            org_id = view.kwargs.get("pk", request.user.get_default_organization().id)
+            return not (models.Role.END_USER == request.user.get_organization_role(org_id) and request.method not in SAFE_METHODS)
         return True
 
 
