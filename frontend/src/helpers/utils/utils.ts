38334df14d3d0658d9700505f88d6a4fff5f1b78
Aftab Ahmad <aftab76ahmad76@gmail.com>
2022-09-23 13:56:49 +0500

    Article page ui updated (#711)
    * Article page ui updated
    
    * Improved logic for adding article
    
    * fix bug
    
    * upload functionality
    
    * formating
    
    Co-authored-by: getalihassan <ali.hassan@prixite.com>

--- frontend/src/helpers/utils/utils.ts
+++ frontend/src/helpers/utils/utils.ts
@@ -56,6 +56,24 @@ const getNonFieldError = (error: unknown) => {
   }
 };
 
+const addIdToHeadings = (htmlString: string): string => {
+  let index = 0;
+
+  const str = htmlString.replace(
+    /<h[1-6].*?>(.*?)/g,
+    (item) => `<h${item[2]} id='${index++}' >`
+  );
+
+  return str;
+};
+
+const convertImages = (htmlText) => {
+  return htmlText.replace(
+    /<div style="text-align:none;"><img/g,
+    '<div style="text-align:center;"><img'
+  );
+};
+
 const isNonFieldError = (error: unknown): boolean => {
   if (isApiError(error)) {
     return isBadRequestError(error) && "non_field_errors" in error.data;
@@ -142,4 +160,6 @@ export {
   getNonFieldError,
   isNonFieldError,
   returnPayloadThemeObject,
+  addIdToHeadings,
+  convertImages,
 };
