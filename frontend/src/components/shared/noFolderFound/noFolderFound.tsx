4aaaceaa6b096f41f5306657f959d035d9ccf309
Faisal Usman <faisal.usman@prixite.com>
2022-09-23 10:51:28 +0500

    Fix issue on categories page.
    RT #699

--- frontend/src/components/shared/noFolderFound/noFolderFound.tsx
+++ frontend/src/components/shared/noFolderFound/noFolderFound.tsx
@@ -1,4 +1,3 @@
-
 import { Grid, Card, CardContent, Typography } from "@mui/material";
 import "@src/components/shared/noFolderFound/noFolderFound.scss";
 
@@ -6,20 +5,17 @@ interface Props {
   errorTitle: string;
 }
 
-const NoFolderFound = ({
-  errorTitle,
-}: Props) => {
+const NoFolderFound = ({ errorTitle }: Props) => {
   return (
-       <Grid item xs={12} xl={12} md={12} lg={12}>
-                <Card className="no-folder-card-div">
-                  <CardContent>
-                    <Typography className="no-folder-error-div" gutterBottom>
-                      {errorTitle}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-      
+    <Grid item xs={12} xl={12} md={12} lg={12}>
+      <Card className="no-folder-card-div">
+        <CardContent>
+          <Typography className="no-folder-error-div" gutterBottom>
+            {errorTitle}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 };
 
