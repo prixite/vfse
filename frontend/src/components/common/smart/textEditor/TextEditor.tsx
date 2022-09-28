fe1030fd3234983c612de18e3d7929cdb91755bf
Aftab Ahmad <aftab76ahmad76@gmail.com>
2022-09-28 14:02:00 +0500

    Improved logic for text editor

--- frontend/src/components/common/smart/textEditor/TextEditor.tsx
+++ frontend/src/components/common/smart/textEditor/TextEditor.tsx
@@ -1,7 +1,6 @@
 import { useEffect, Dispatch, SetStateAction } from "react";
 
-import { EditorState, ContentState, RichUtils } from "draft-js";
-import { insertNewUnstyledBlock } from "draftjs-utils";
+import { EditorState, ContentState } from "draft-js";
 import htmlToDraft from "html-to-draftjs";
 import { Editor } from "react-draft-wysiwyg";
 
@@ -52,22 +51,6 @@ const TextEditor = ({ htmlText, editorState, setEditorState }: text) => {
 
   return (
     <Editor
-      handleReturn={(event) => {
-        // override behavior for enter key
-        let newEditorState = null;
-        if (event.keyCode === 13 && event.shiftKey) {
-          // with shift, make a new block
-          newEditorState = insertNewUnstyledBlock(editorState);
-        } else if (event.keyCode === 13 && !event.shiftKey) {
-          // without shift, just a normal line break
-          newEditorState = RichUtils.insertSoftNewline(editorState);
-        }
-        if (newEditorState) {
-          setEditorState(newEditorState);
-          return true;
-        }
-        return false;
-      }}
       editorState={editorState}
       editorClassName="editor"
       onEditorStateChange={onTextChange}
