2386049ad278066a8d4736f880d150e35b26de89
Faisal Usman <faisal.usman@prixite.com>
2022-10-11 14:54:17 +0500

    Reslove feedback changes.
    RT #728

--- frontend/src/components/common/presentational/backBtn/BackBtn.tsx
+++ frontend/src/components/common/presentational/backBtn/BackBtn.tsx
@@ -1,5 +1,5 @@
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
-import { useNavigate, useLocation } from "react-router";
+import { useNavigate } from "react-router";
 
 import { constants } from "@src/helpers/utils/constants";
 const { organizationRoute } = constants;
@@ -11,19 +11,16 @@ import "@src/components/common/presentational/backBtn/backBtn.scss";
 const BackBtn = () => {
   const selectedOrganization = useSelectedOrganization();
   const { articleDescription } = constantsData;
-  const history = useLocation();
   const navigate = useNavigate();
   return (
     <div className="back-btn">
       <div
         className="back-btn-container"
-        onClick={() => {
-          history.pathname.includes(vfseRoutes[0].path)
-            ? navigate(
-                `/${organizationRoute}/${selectedOrganization?.id}${vfseRoutes[0].path}`
-              )
-            : navigate(-1);
-        }}
+        onClick={() =>
+          navigate(
+            `/${organizationRoute}/${selectedOrganization?.id}${vfseRoutes[0].path}`
+          )
+        }
       >
         <ArrowRightAltIcon
           style={{ transform: "rotate(180deg)", color: "rgb(0, 0, 0)" }}
