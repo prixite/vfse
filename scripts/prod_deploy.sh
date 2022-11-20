53c1834d744bb4870dcce0e4f4554da4f02cfed2
Umair Khan <umair.khan@toptal.com>
2022-11-20 10:59:47 +0500

    Use prod

--- scripts/prod_deploy.sh
+++ scripts/prod_deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sudo docker-compose -f docker-compose.staging.yml build;
-sudo docker-compose -f docker-compose.staging.yml up -d;
-sudo docker-compose -f docker-compose.staging.yml exec app ./manage.py migrate;
-sudo docker-compose -f docker-compose.staging.yml exec app npm run build;
-sudo docker-compose -f docker-compose.staging.yml exec app ./manage.py collectstatic --noinput;
+sudo docker-compose -f docker-compose.prod.yml build;
+sudo docker-compose -f docker-compose.prod.yml up -d;
+sudo docker-compose -f docker-compose.prod.yml exec app ./manage.py migrate;
+sudo docker-compose -f docker-compose.prod.yml exec app npm run build;
+sudo docker-compose -f docker-compose.prod.yml exec app ./manage.py collectstatic --noinput;
