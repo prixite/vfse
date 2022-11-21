969f5b4ade7b8311ca04447961f75d36b2df7181
Umair Khan <umair.khan@toptal.com>
2022-11-21 13:34:07 +0500

    Fix frontend linting error

--- frontend/src/helpers/utils/imageUploadUtils.ts
+++ frontend/src/helpers/utils/imageUploadUtils.ts
@@ -6,7 +6,7 @@ const config = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   s3Url: `https://${process.env.AWS_STORAGE_BUCKET_NAME}.s3.${process.env.AWS_DEFAULT_REGION}.amazonaws.com`,
-  dirName: 'media',
+  dirName: "media",
 };
 
 const ReactS3Client = new S3(config);
