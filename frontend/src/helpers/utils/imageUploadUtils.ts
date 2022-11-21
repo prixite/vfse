e76b5d0cd594d5920a0eb651618673d83eb5d326
Umair Khan <umair.khan@toptal.com>
2022-11-21 12:49:54 +0500

    Use media folder

--- frontend/src/helpers/utils/imageUploadUtils.ts
+++ frontend/src/helpers/utils/imageUploadUtils.ts
@@ -5,6 +5,8 @@ const config = {
   region: process.env.AWS_DEFAULT_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  s3Url: `https://${process.env.AWS_STORAGE_BUCKET_NAME}.s3.${process.env.AWS_DEFAULT_REGION}.amazonaws.com`,
+  dirName: 'media',
 };
 
 const ReactS3Client = new S3(config);
