e48a8bc1cd708c3dbdaf851831b92c049406ab86
Umair Khan <umair.khan@toptal.com>
2022-11-24 12:42:54 +0500

    Rename variable

--- core/management/commands/cron.py
+++ core/management/commands/cron.py
@@ -15,10 +15,10 @@ class Command(BaseCommand):
             headers=utils.CRADLEPOINT_REQUEST_HEADERS,
         )
         for router in response.json()["data"]:
-            last_known_location = router["last_known_location"]
-            if last_known_location:
+            location_url = router["last_known_location"]
+            if location_url:
                 location_request = requests.get(
-                    url=last_known_location, headers=utils.CRADLEPOINT_REQUEST_HEADERS
+                    url=location_url, headers=utils.CRADLEPOINT_REQUEST_HEADERS
                 )
                 location = location_request.json()
                 utils.post_data_to_influxdb(
