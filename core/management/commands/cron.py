58d5126a683bd7f3d9c77f0f8bb5f063ad8f11a0
Ali Hassan <94823010+getalihassan@users.noreply.github.com>
2022-11-28 14:12:32 +0500

    fix missing router bug and status tag (#857)
    * fix pagination bug
    
    * remove print statement
    
    * remove print statement
    
    * Improve code
    
    Co-authored-by: Umair Khan <umair.khan@toptal.com>

--- core/management/commands/cron.py
+++ core/management/commands/cron.py
@@ -10,22 +10,32 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
-        response = requests.get(
-            url=f"{utils.CRADLEPOINT_API_URL}/routers",
-            headers=utils.CRADLEPOINT_REQUEST_HEADERS,
-        )
-        for router in response.json()["data"]:
-            location_url = router["last_known_location"]
-            if location_url:
-                location_request = requests.get(
-                    url=location_url, headers=utils.CRADLEPOINT_REQUEST_HEADERS
-                )
-                location = location_request.json()
-                utils.post_gps_data_to_influxdb(
-                    location["longitude"],
-                    location["latitude"],
-                    router["name"],
-                )
-        self.stdout.write(
-            self.style.SUCCESS("Successfully posted to Influx.")
-        )
+        offset = 0
+
+        next_page = True
+        while next_page:
+            response = requests.get(
+                url=f"{utils.CRADLEPOINT_API_URL}/routers/?offset={offset}",
+                headers=utils.CRADLEPOINT_REQUEST_HEADERS,
+            )
+            router_response = response.json()
+            router_list = router_response["data"]
+
+            for router in router_list:
+                location_url = router["last_known_location"]
+                if location_url:
+                    location_request = requests.get(
+                        url=location_url, headers=utils.CRADLEPOINT_REQUEST_HEADERS
+                    )
+                    location = location_request.json()
+                    utils.post_gps_data_to_influxdb(
+                        location["longitude"],
+                        location["latitude"],
+                        router["name"],
+                        router["state"],
+                    )
+
+            next_page = bool(router_response["meta"]["next"])  # check for next page
+            offset += 20  # fetch next 20 items
+
+        self.stdout.write(self.style.SUCCESS("Successfully posted to Influx."))
