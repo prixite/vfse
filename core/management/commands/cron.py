b695b8e2a3ac4d80386d08e5b6e81b5a154c39d9
Umair Khan <umair.khan@toptal.com>
2022-11-28 15:13:09 +0500

    Fix cron job (#858)
    * Fix cron job
    
    * fix
    
    * fix
    
    Co-authored-by: getalihassan <ali.hassan@prixite.com>

--- core/management/commands/cron.py
+++ core/management/commands/cron.py
@@ -11,31 +11,27 @@ class Command(BaseCommand):
     @transaction.atomic
     def handle(self, *args, **options):
         offset = 0
-
         next_page = True
         while next_page:
             response = requests.get(
-                url=f"{utils.CRADLEPOINT_API_URL}/routers/?offset={offset}",
+                url=f"{utils.CRADLEPOINT_API_URL}/locations/?offset={offset}",
                 headers=utils.CRADLEPOINT_REQUEST_HEADERS,
             )
-            router_response = response.json()
-            router_list = router_response["data"]
+            routers_location_response = response.json()
+            routers_location_list = routers_location_response["data"]
 
-            for router in router_list:
-                location_url = router["last_known_location"]
-                if location_url:
-                    location_request = requests.get(
-                        url=location_url, headers=utils.CRADLEPOINT_REQUEST_HEADERS
-                    )
-                    location = location_request.json()
-                    utils.post_gps_data_to_influxdb(
-                        location["longitude"],
-                        location["latitude"],
-                        router["name"],
-                        router["state"],
-                    )
-
-            next_page = bool(router_response["meta"]["next"])  # check for next page
+            for location in routers_location_list:
+                router_request = requests.get(
+                    url=location["router"], headers=utils.CRADLEPOINT_REQUEST_HEADERS
+                )
+                router = router_request.json()
+                utils.post_gps_data_to_influxdb(
+                    location["longitude"],
+                    location["latitude"],
+                    router["name"],
+                    router["state"],
+                )
+            next_page = bool(routers_location_response["meta"]["next"])  # noqa
             offset += 20  # fetch next 20 items
 
         self.stdout.write(self.style.SUCCESS("Successfully posted to Influx."))
