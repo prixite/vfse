648308ee9d5f9bbf4bf398f2097036e5f137212d
Umair Khan <umair.khan@toptal.com>
2022-12-09 12:49:36 +0500

    Fix linter

--- frontend/src/components/shared/layout/sideBar/SideBar.tsx
+++ frontend/src/components/shared/layout/sideBar/SideBar.tsx
@@ -15,7 +15,7 @@ import {
   Collapse,
 } from "@mui/material";
 import { styled, Theme, CSSObject } from "@mui/material/styles";
-import { useNavigate , Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 import CloseBtn from "@src/assets/images/down.png";
 import OpenBtn from "@src/assets/images/opendrawer.png";
