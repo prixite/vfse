b7b07eca9bd16116f43657160f7a25962f9667cc
Umair Khan <umair.khan@toptal.com>
2023-02-03 13:24:15 +0500

    Update core/views/api.py

--- core/views/api.py
+++ core/views/api.py
@@ -364,7 +364,6 @@ class SystemVncUrlViewSet(ModelViewSet, mixins.UserOganizationMixin):
         return queryset
 
     def retrieve(self, request, *args, **kwargs):
-        print(self.queryset)
         if not self.get_object().connection_options["vfse"]:
             raise Http404("VNC access is not allowed")
         return super().retrieve(request, *args, **kwargs)
