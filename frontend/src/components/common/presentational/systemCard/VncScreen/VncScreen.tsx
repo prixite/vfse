82037592154c0b6c6e935edf154ad626567c38fd
Umair Khan <umair.khan@toptal.com>
2023-02-26 19:45:48 +0500

    Handle mouse and keyboard

--- frontend/src/components/common/presentational/systemCard/VncScreen/VncScreen.tsx
+++ frontend/src/components/common/presentational/systemCard/VncScreen/VncScreen.tsx
@@ -17,13 +17,15 @@ export function VncScreen({ system, username, password }: VncScreenProp) {
   const width = 1024;
   const height = 768;
 
-  const client = useRef(null);
+  const guac = useRef(null);
+  const keyboard = useRef(null);
+  const mouse = useRef(null);
   const displayRef = useRef(null);
 
   useEffect(() => {
-    client.current = new Guacamole.Client(new WebSocketTunnel(tunnelURL));
-    displayRef.current.appendChild(client.current.getDisplay().getElement());
-    client.current.connect(
+    guac.current = new Guacamole.Client(new WebSocketTunnel(tunnelURL));
+    displayRef.current.appendChild(guac.current.getDisplay().getElement());
+    guac.current.connect(
       [
         `guacd_host=${GUACD_IP}`,
         `guacd_port=${GUACD_PORT}`,
@@ -37,8 +39,30 @@ export function VncScreen({ system, username, password }: VncScreenProp) {
         `dpi=96`,
       ].join("&")
     );
+
+    // Mouse
+    mouse.current = new Guacamole.Mouse(guac.current.getDisplay().getElement());
+
+    mouse.current.onmousedown =
+      mouse.current.onmouseup =
+      mouse.current.onmousemove =
+        function (mouseState) {
+          guac.current.sendMouseState(mouseState);
+        };
+
+    // Keyboard
+    keyboard.current = new Guacamole.Keyboard(document);
+
+    keyboard.current.onkeydown = function (keysym) {
+      guac.current.sendKeyEvent(1, keysym);
+    };
+
+    keyboard.current.onkeyup = function (keysym) {
+      guac.current.sendKeyEvent(0, keysym);
+    };
+
     return function cleanup() {
-      client.current.disconnect();
+      guac.current.disconnect();
     };
   }, []);
 
@@ -54,87 +78,3 @@ export function VncScreen({ system, username, password }: VncScreenProp) {
     />
   );
 }
-
-/*
-export function VncScreen() {
-  const client = useRef();
-  const keyboard = useRef();
-  const mouse = useRef();
-
-  const disconnect = () => {
-    client.current.disconnect();
-    document.getElementById("display").innerHTML = "";
-    keyboard.current = null;
-    mouse.current = null;
-    client.current = null;
-  };
-
-  const connect = () => {
-    const hostname = document.getElementById("guacd-host").value;
-    const port = document.getElementById("guacd-port").value;
-    const protocol = document.getElementById("protocol").value;
-    const remoteHost = document.getElementById("remote-host").value;
-    const remotePort = document.getElementById("remote-port").value;
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    const width = document.getElementById("width").value;
-    const height = document.getElementById("height").value;
-    const dpi = document.getElementById("dpi").value;
-    const tunnel = new Guacamole.WebSocketTunnel(
-      "ws://localhost:8000/websocket/"
-    );
-    const guac = (client.current = new Guacamole.Client(tunnel));
-    document.getElementById("display").appendChild(guac.getDisplay().getElement());
-
-    guac.onstatechange = (state) => {
-      if (state === 5) {
-        disconnect();
-      }
-    };
-
-    guac.connect(
-      [
-        `guacd_host=${hostname}`,
-        `guacd_port=${port}`,
-        `protocol=${protocol}`,
-        `remote_host=${remoteHost}`,
-        `remote_port=${remotePort}`,
-        `username=${username}`,
-        `password=${password}`,
-        `width=${width}`,
-        `height=${height}`,
-        `dpi=${dpi}`,
-      ].join("&")
-    );
-
-    window.onunload = function () {
-      disconnect();
-    };
-
-    // Mouse
-    mouse.current = new Guacamole.Mouse(guac.getDisplay().getElement());
-
-    mouse.current.onmousedown =
-      mouse.current.onmouseup =
-      mouse.current.onmousemove =
-        function (mouseState) {
-          guac.sendMouseState(mouseState);
-        };
-
-    // Keyboard
-    keyboard.current = new Guacamole.Keyboard(document);
-
-    keyboard.current.onkeydown = function (keysym) {
-      guac.sendKeyEvent(1, keysym);
-    };
-
-    keyboard.current.onkeyup = function (keysym) {
-      guac.sendKeyEvent(0, keysym);
-    };
-  };
-
-  return <div id="display"></div>;
-}
-
-export default VncScreen;
-*/
