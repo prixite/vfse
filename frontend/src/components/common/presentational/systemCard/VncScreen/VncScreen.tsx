863b70fe01f0488a8a15fc77f08fe658f52fa46e
Umair Khan <umair.khan@toptal.com>
2023-02-27 10:02:04 +0500

    Remove extra code

--- frontend/src/components/common/presentational/systemCard/VncScreen/VncScreen.tsx
+++ frontend/src/components/common/presentational/systemCard/VncScreen/VncScreen.tsx
@@ -22,26 +22,6 @@ export function VncScreen({ system, username, password }: VncScreenProp) {
   const mouse = useRef(null);
   const displayRef = useRef(null);
 
-  setTimeout(() => {
-    // get current width/height of connection
-    let remoteDisplayWidth = guac.current.getDisplay().getWidth();
-    let remoteDisplayHeight = guac.current.getDisplay().getHeight();
-
-    if (!displayRef.current) {
-      return;
-    }
-
-    let newWidth = displayRef.current.clientWidth;
-    let newHeight = displayRef.current.clientHeight;
-
-    // calculate which scale should we use - width or height, in order to see all of remote display
-    let newScale = Math.min(newWidth / remoteDisplayWidth, newHeight / remoteDisplayHeight, 1);
-
-    console.log(newWidth, newHeight, remoteDisplayWidth, remoteDisplayHeight);
-    guac.current.sendSize(1024, 768)
-    //guac.current.getDisplay().scale(newScale);
-  }, 10 * 1000);
-
   useEffect(() => {
     guac.current = new Guacamole.Client(new WebSocketTunnel(tunnelURL));
     displayRef.current.appendChild(guac.current.getDisplay().getElement());
@@ -59,10 +39,6 @@ export function VncScreen({ system, username, password }: VncScreenProp) {
         `dpi=96`,
       ].join("&")
     );
-
-    guac.current.onresize = (e) => {
-      console.log(e);
-    }
 
     // Mouse
     mouse.current = new Guacamole.Mouse(guac.current.getDisplay().getElement());
