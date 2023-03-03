6a5ae15f550b369ea8c6516b15eb84980ca2a0c6
Umair Khan <umair.khan@toptal.com>
2023-03-03 13:19:12 +0500

    Implement tcp proxy

--- guac.py
+++ guac.py
@@ -67,3 +67,27 @@ async def websocket_endpoint(
         await client.close()
 
     await asyncio.wait([task])
+
+
+async def vnc_to_ws(websocket: WebSocket, reader):
+    while True:
+        data = await reader.read(2**16)
+        await websocket.send_bytes(data)
+
+
+@app.websocket("/sockify/")
+async def raw_websocket(websocket: WebSocket, host: str, port: str):
+    await websocket.accept()
+    reader, writer = await asyncio.open_connection(host, port)
+    logging.info(f"Connection with {host}:{port} established")
+    task = asyncio.get_event_loop().create_task(vnc_to_ws(websocket, reader))
+
+    try:
+        while True:
+            data = await websocket.receive_bytes()
+            writer.write(data)
+            await writer.drain()
+    except WebSocketDisconnect:
+        task.cancel()
+
+    await asyncio.wait([task])
