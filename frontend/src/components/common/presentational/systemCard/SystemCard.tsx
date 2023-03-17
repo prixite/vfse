ce7ff9dd657b4e8f3ec6dbcdf54c2a2fe543b5bc
Umair Khan <umair.khan@toptal.com>
2023-03-17 15:17:40 +0500

    Remove variable

--- frontend/src/components/common/presentational/systemCard/SystemCard.tsx
+++ frontend/src/components/common/presentational/systemCard/SystemCard.tsx
@@ -30,7 +30,7 @@ import Machine from "@src/assets/images/system.png";
 import useStyles from "@src/components/common/presentational/systemCard/Style";
 import ConfirmationModal from "@src/components/shared/popUps/confirmationModal/ConfirmationModal";
 import { SystemInterfaceProps } from "@src/helpers/interfaces/localizationinterfaces";
-import { timeOut, constants } from "@src/helpers/utils/constants";
+import { timeOut } from "@src/helpers/utils/constants";
 import { localizedData } from "@src/helpers/utils/language";
 import { toastAPIError } from "@src/helpers/utils/utils";
 import constantsData from "@src/localization/en.json";
@@ -127,7 +127,6 @@ const SystemCard = ({
     comments,
     deleteText,
   } = systemCard;
-  const { organizationRoute } = constants;
   const handleModalClose = () => {
     setOpenModal(false);
   };
@@ -499,10 +498,7 @@ const SystemCard = ({
                 {system.connection_options.service_web_browser && (
                   <MenuItem
                     onClick={() =>
-                      window.open(
-                        `/htmlproxy/${system.id}/service/`,
-                        "_blank"
-                      )
+                      window.open(`/htmlproxy/${system.id}/service/`, "_blank")
                     }
                   >
                     <span style={{ marginLeft: "12px" }}>
