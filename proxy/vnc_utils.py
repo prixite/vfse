5edcd43ef15f4f6e0ab23d5374a7b3a092c50ce6
Umair Khan <umair.khan@toptal.com>
2023-04-26 21:45:55 +0500

    Handle multiple connections

--- proxy/vnc_utils.py
+++ proxy/vnc_utils.py
@@ -1,9 +1,10 @@
 import asyncio
 import logging
+from collections import defaultdict
 
 from asgiref.sync import sync_to_async
 from django.conf import settings
-from fastapi import WebSocket, WebSocketDisconnect
+from fastapi import HTTPException, WebSocket, WebSocketDisconnect
 
 from core.models import System
 from proxy.telnetlib import Telnet
@@ -56,23 +57,52 @@ def start_vnc_server_using_telnet(telnet: Telnet, system: System):
 
     assert system.vnc_server_path
     telnet.write(b"cd " + system.vnc_server_path.encode("ascii") + b"\n")
-    telnet.write(b"./gemsvnc\n")
-    telnet.read_until(b"GEMS VNC Server ready")
-    telnet.msg("GEMS VNC started")
+    telnet.write(b"./gemsvnc -shared\n")
+    index, _, text = telnet.expect(
+        [
+            b"GEMS VNC Server ready",
+            b"Address already in use",
+        ],
+        timeout=5,
+    )
+    if index == -1:
+        telnet.msg(text)
+        raise HTTPException(status_code=500, detail="Could not start server")
+    else:
+        telnet.msg("GEMS VNC started")
 
 
 class TelnetContextManager:
+    system_connections = defaultdict(int)
+    telnet_connections = {}
+
     def __init__(self, system: System):
         self.system = system
-        self.telnet = Telnet()
-        self.telnet.set_debuglevel(settings.TELNET_LOG_LEVEL)
 
     async def __aenter__(self):
-        await start_vnc_server_using_telnet(self.telnet, self.system)
+        if self.system_connections[self.system.id] == 0:
+            telnet = Telnet()
+            telnet.set_debuglevel(settings.TELNET_LOG_LEVEL)
+
+            await start_vnc_server_using_telnet(telnet, self.system)
+
+            self.telnet_connections[self.system.id] = telnet
+
+        self.system_connections[self.system.id] += 1
+        if settings.TELNET_LOG_LEVEL:
+            print(
+                f"Connections to {self.system.ip_address} = "
+                f"{self.system_connections[self.system.id]}"
+            )
 
     async def __aexit__(self, exc_type, exc, tb):
         @sync_to_async
         def close():
-            self.telnet.close()
+            self.telnet_connections[self.system.id].close()
+            if settings.TELNET_LOG_LEVEL:
+                print(f"Closing telnet connection to {self.system.ip_address}")
 
-        await close()
+        self.system_connections[self.system.id] -= 1
+        if self.system_connections[self.system.id] <= 0:
+            await close()
+            self.system_connections[self.system.id] = 0
