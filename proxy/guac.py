7284c9d5ee4cbf0798cb949934ef6b131f5bfe76
Umair Khan <umair.khan@toptal.com>
2023-05-09 12:59:09 +0500

    Add guac proxy for telnet (#1126)
    * Add back guacuamole.
    
    Ref commit: 2f23e2219758be6af62c42b75dd93648ced2fd33
    
    * Add back html_proxy and vnc_proxy
    
    * Move guacamole code to proxy
    
    * Get the required code
    
    * Add guac in docker-compose.yml
    
    * Fix errors

---
+++ proxy/guac.py
@@ -0,0 +1,69 @@
+import asyncio
+import logging
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+from proxy.guacamole.client import GuacamoleClient
+from proxy.guacamole.instruction import Instruction
+
+app = FastAPI()
+
+
+async def guacd_to_client(websocket: WebSocket, client: GuacamoleClient):
+    while True:
+        instruction = await client.read()
+        if instruction.error:
+            logging.error(f"{instruction.short_description}-{instruction.description}")
+        await websocket.send_text(str(instruction))
+
+
+@app.get("/")
+def index():
+    return {"Hello": "World"}
+
+
+@app.websocket("/websocket/")
+async def websocket_endpoint(
+    websocket: WebSocket,
+    guacd_host: str,
+    guacd_port: str,
+    protocol: str,
+    remote_host: str,
+    remote_port: str,
+    width: str,
+    height: str,
+    dpi: str,
+):
+    await websocket.accept(subprotocol="guacamole")
+    client = GuacamoleClient(
+        guacd_host,
+        guacd_port,
+        {
+            "protocol": protocol,
+            "size": [width, height, dpi],
+            "audio": [],
+            "video": [],
+            "image": [],
+            "args": {
+                "hostname": remote_host,
+                "port": remote_port,
+            },
+        },
+        debug=False,
+    )
+    await client.connect()
+    await client.handshake(websocket)
+    logging.info("Handshake complete")
+
+    task = asyncio.get_event_loop().create_task(guacd_to_client(websocket, client))
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            instruction = Instruction.from_string(data)
+            await client.send(str(instruction))
+    except WebSocketDisconnect:
+        task.cancel()
+        await client.close()
+
+    await asyncio.wait([task])
