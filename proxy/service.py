8f42fc58ca67a1f78aaeb5ac42e80fd5b6816ec3
Umair Khan <umair.khan@toptal.com>
2023-05-30 11:42:24 +0500

    Start work on proxy

--- proxy/service.py
+++ proxy/service.py
@@ -16,14 +16,17 @@ def health(request: Request):
     return "OK"
 
 
-@app.get("/{organization_id:int}/{system_id:int}/{path:path}")
-async def index_proxy(
-    organization_id: int, system_id: int, path: str, request: Request
-):
+def get_system_info_from_hostname(request: Request):
+    pass
+
+
+@app.get("/{path:path}")
+async def index_proxy(path: str, request: Request):
     if not is_authenticated(request):
         raise HTTPException(status_code=403, detail="Not authenticated")
 
     user = await get_user_from_request(request)
+    organization_id, system_id = get_system_info_from_hostname(request)
     system = await get_system(user, organization_id, system_id)
 
     if not system.connection_options.get("service_web_browser"):
@@ -31,71 +34,9 @@ async def index_proxy(
             status_code=400, detail="No service browser access for system"
         )
 
-    if not path.startswith("service"):
-        path = f"service/{path}"
-
     url = f"http://{system.ip_address}/{path}"
     async with httpx.AsyncClient() as client:
         proxy = await client.get(url)
-
-    content = proxy.content.replace(
-        b"/service/",
-        settings.HTML_PROXY_PATH.encode()
-        + str(organization_id).encode()
-        + b"/"
-        + str(system_id).encode()
-        + b"/service/",
-    )
-
-    keys = [
-        b"dd/site.htm",
-        b"dd/lastkey.htm",
-        b"global/execform.htm",
-        b"global/stopform.htm",
-        b"global/terminateform.htm",
-        b"global/debugform.htm",
-        b"ggjscript/ggjscript.htm",
-        b"access/access.htm",
-        b"homemenu/homemenu.stm",
-    ]
-
-    if settings.HTML_PROXY_PATH == "/":
-        for key in keys:
-            content = content.replace(key, str(system_id).encode() + b"/" + key)
-    else:
-        for key in keys:
-            content = content.replace(
-                key,
-                str(organization_id).encode()
-                + b"/"
-                + str(system_id).encode()
-                + b"/"
-                + key,
-            )
-
-    proxy.headers.update({"content-length": str(len(content))})
-    response = Response(content=content)
-    response.headers.update(proxy.headers)
-    response.status_code = proxy.status_code
-    return response
-
-
-@app.post("/{organization_id:int}/{system_id:int}/{path:path}")
-async def post_proxy(organization_id: int, system_id: int, path: str, request: Request):
-    if not is_authenticated(request):
-        raise HTTPException(status_code=403, detail="Not authenticated")
-
-    user = await get_user_from_request(request)
-    system = await get_system(user, organization_id, system_id)
-
-    if not path.startswith("service"):
-        path = f"service/{path}"
-
-    url = f"http://{system.ip_address}/{path}"
-
-    data = await request.form()
-    async with httpx.AsyncClient() as client:
-        proxy = await client.post(url, data=dict(data))
 
     response = Response(content=proxy.content)
     response.headers.update(proxy.headers)
