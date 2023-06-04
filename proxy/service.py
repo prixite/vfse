6fd283ff08509e2e2346b0c9db9bfd7b2743677a
Umair Khan <umair.khan@toptal.com>
2023-06-04 21:00:36 +0500

    Abstract proxy logic

--- proxy/service.py
+++ proxy/service.py
@@ -29,8 +29,7 @@ def to_base_url(url: str):
     return f"{result.scheme}://{result.netloc}"
 
 
-@app.get("/{path:path}")
-async def get_proxy(path: str, request: Request):
+async def proxy(method_name: str, path: str, request: Request, data=None):
     if not is_authenticated(request):
         raise HTTPException(status_code=403, detail="Not authenticated")
 
@@ -45,34 +44,23 @@ async def get_proxy(path: str, request: Request):
 
     base_url = to_base_url(system.safe_service_page_url)
     async with httpx.AsyncClient() as client:
-        proxy = await client.get(f"{base_url}/{path}")
+        method = getattr(client, method_name)
+        if data is None:
+            proxy = await method(f"{base_url}/{path}")
+        else:
+            proxy = await method(f"{base_url}/{path}", data=dict(data))
 
     response = Response(content=proxy.content)
     response.headers.update(proxy.headers)
     response.status_code = proxy.status_code
     return response
+
+
+@app.get("/{path:path}")
+async def get_proxy(path: str, request: Request):
+    return await proxy("get", path, request)
 
 
 @app.post("/{path:path}")
 async def post_proxy(path: str, request: Request):
-    if not is_authenticated(request):
-        raise HTTPException(status_code=403, detail="Not authenticated")
-
-    user = await get_user_from_request(request)
-    organization_id, system_id = get_system_info_from_hostname(request)
-    system = await get_system(user, organization_id, system_id)
-
-    if not system.connection_options.get("service_web_browser"):
-        raise HTTPException(
-            status_code=400, detail="No service browser access for system"
-        )
-
-    data = await request.form()
-    base_url = to_base_url(system.safe_service_page_url)
-    async with httpx.AsyncClient() as client:
-        proxy = await client.post(f"{base_url}/{path}", data=dict(data))
-
-    response = Response(content=proxy.content)
-    response.headers.update(proxy.headers)
-    response.status_code = proxy.status_code
-    return response
+    return await proxy("post", path, request, request.form())
