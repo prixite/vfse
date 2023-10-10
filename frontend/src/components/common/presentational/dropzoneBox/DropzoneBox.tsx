14dcaf5ac353f613b6557bb0d6fc547730f41dcf
shameenjamil <135616916+shameenjamil@users.noreply.github.com>
2023-10-10 11:15:41 +0500

    upload up to 20mbs and display errors (#1351)
    * upload up to 20mbs and display errors
    
    * added notifications via library and code modification

--- frontend/src/components/common/presentational/dropzoneBox/DropzoneBox.tsx
+++ frontend/src/components/common/presentational/dropzoneBox/DropzoneBox.tsx
@@ -3,10 +3,12 @@ import { SetStateAction, useEffect, Dispatch } from "react";
 import "@src/components/common/presentational/dropzoneBox/dropzoneBox.scss";
 import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
-import { useDropzone } from "react-dropzone";
+import { useDropzone, FileRejection } from "react-dropzone";
 import { useTranslation } from "react-i18next";
+import { toast } from "react-toastify";
 
 import UploadBtn from "@src/assets/svgs/upload-icon.svg";
+import { timeOut } from "@src/helpers/utils/constants";
 import { useAppSelector } from "@src/store/hooks";
 interface DropzoneProps {
   setSelectedImage: Dispatch<SetStateAction<unknown[]>>;
@@ -15,25 +17,29 @@ interface DropzoneProps {
   selectedImage?: File[];
 }
 
+const MAX_FILE_SIZE = 20 * 1024 * 1024;
+
 const DropzoneBox = ({
   setSelectedImage,
   selectedImage,
   imgSrc,
   isUploading,
 }: DropzoneProps) => {
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
-    useFsAccessApi: false, //permissions issue in linux (known bug)
-    accept: {
-      "image/png": [".jpg", ".jpeg", ".png", ".gif"],
-    },
-    onDrop: (acceptedFiles) => {
-      return acceptedFiles.map((file) =>
-        Object.assign(file, {
-          preview: URL.createObjectURL(file),
-        })
-      );
-    },
-  });
+  const { acceptedFiles, getRootProps, getInputProps, fileRejections } =
+    useDropzone({
+      useFsAccessApi: false,
+      accept: {
+        "image/png": [".jpg", ".jpeg", ".png", ".gif"],
+      },
+      maxSize: MAX_FILE_SIZE,
+      onDrop: (acceptedFiles) => {
+        return acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        );
+      },
+    });
 
   const { t } = useTranslation();
   const { buttonBackground, buttonTextColor } = useAppSelector(
@@ -49,6 +55,37 @@ const DropzoneBox = ({
       setSelectedImage(acceptedFiles);
     }
   }, [acceptedFiles]);
+
+  useEffect(() => {
+    if (fileRejections?.length) {
+      const invalidFormatFiles = fileRejections.filter(
+        (rejection: FileRejection) =>
+          rejection.errors.some((error) => error.code === "file-invalid-type")
+      );
+
+      const oversizedFiles = fileRejections.filter((rejection: FileRejection) =>
+        rejection.errors.some((error) => error.code === "file-too-large")
+      );
+
+      if (invalidFormatFiles.length > 0) {
+        toast.success(
+          "One or more files have an invalid format. Only JPG, JPEG, PNG, and GIF formats are allowed.",
+          {
+            autoClose: timeOut,
+            pauseOnHover: false,
+          }
+        );
+      }
+
+      if (oversizedFiles.length > 0) {
+        toast.success("One or more files exceed the maximum size of 20MB.", {
+          autoClose: timeOut,
+          pauseOnHover: false,
+        });
+      }
+    }
+  }, [fileRejections]);
+
   return (
     <section className="dropzone-style" {...getRootProps()}>
       <input {...getInputProps()} />
